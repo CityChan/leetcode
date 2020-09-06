89e98f96146f9d291c7d8e615498b2de933c80c4
CityChan <53466807+CityChan@users.noreply.github.com>
2020-09-06 22:47:21 +0000

    [LeetCode Sync] Runtime - 2 ms (43.71%), Memory - 38.1 MB (100.00%)

--- my-folder/0067-add-binary/solution.java
+++ my-folder/0067-add-binary/solution.java
@@ -1,22 +1,25 @@
 class Solution {
     public String addBinary(String a, String b) {
-        int carry = 0;
         StringBuilder sb = new StringBuilder();
-        int i = a.length() - 1;
-        int j = b.length() - 1;
-        while (i >= 0 || j >= 0 || carry > 0) {
+        int i = a.length()-1;
+        int j = b.length()-1;
+        int carry = 0;
+        while(i>=0||j>=0){
             int sum = carry;
-            if (i >= 0) {
+            if(i>=0){
                 sum += a.charAt(i) - '0';
                 i--;
             }
-            if (j >= 0) {
+            if(j>=0){
                 sum += b.charAt(j) - '0';
                 j--;
             }
+            sb.append(sum%2);
             carry = sum / 2;
-            sb.insert(0, sum % 2);
         }
-        return sb.toString();
+        if(carry==1){
+            sb.append(carry);
+        }
+        return sb.reverse().toString();
     }
 }
