2b59945fff83c1e170d1a65101c4a556f6d092b4
CityChan <53466807+CityChan@users.noreply.github.com>
2020-09-06 06:44:24 +0000

    [LeetCode Sync] Runtime - 2 ms (100.00%), Memory - 39.7 MB (100.00%)

--- my-folder/0523-continuous-subarray-sum/solution.java
+++ my-folder/0523-continuous-subarray-sum/solution.java
@@ -2,15 +2,16 @@ class Solution {
     public boolean checkSubarraySum(int[] nums, int k) {
         Map<Integer, Integer> map = new HashMap<>();
         map.put(0, -1);
-        int runningSum = 0;
+        int prefixSum = 0;
         for (int i = 0; i < nums.length; i++) {
-            runningSum += nums[i];
-            if (k != 0) runningSum %= k;
-            Integer prev = map.get(runningSum);
-            if (prev != null) {
-                if (i - prev > 1) return true;
+            prefixSum += nums[i];
+            if (k != 0) prefixSum %= k;
+            if (map.containsKey(prefixSum)) {
+                if (i - map.get(prefixSum) > 1) {
+                    return true;
+                }
             } else {
-                map.put(runningSum, i);
+                map.put(prefixSum, i);
             }
         }
         return false;
