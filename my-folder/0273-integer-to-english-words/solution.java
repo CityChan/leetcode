84e45ea4210ffb2d39a1fe264a86355bc9af6f10
CityChan <53466807+CityChan@users.noreply.github.com>
2020-10-04 21:43:08 +0000

    [LeetCode Sync] Runtime - 9 ms (46.44%), Memory - 39.2 MB (100.00%)

--- my-folder/0273-integer-to-english-words/solution.java
+++ my-folder/0273-integer-to-english-words/solution.java
@@ -1,35 +1,46 @@
 class Solution {
-    private final String[] LESS_THAN_TWENTY= {
-        "", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine",
-        "Ten", "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen", "Eighteen", "Nineteen"};
-    private final String[] TENS = {
-        "", "Ten", "Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety"};
-    private final int TEN = 10;
-    private final int HUNDRED = 100;
-    private final int THOUSAND = 1000;
-    private final int MILLION = 1000000;
-    private final int BILLION = 1000000000;
+    String[] lessThan20 = {"", "One", "Two", "Three","Four","Five", "Six","Seven","Eight","Nine",
+                    "Ten", "Eleven", "Twelve", "Thirteen","Fourteen","Fifteen", "Sixteen","Seventeen","Eighteen","Nineteen"};
+    String[] tens = {"", "", "Twenty", "Thirty","Forty","Fifty", "Sixty","Seventy","Eighty","Ninety"};
+    String[] levels = {"", " Thousand", " Million"," Billion"};
     
     public String numberToWords(int num) {
-        if (num == 0) return "Zero";
-        return helper(num);
-    }
-    
-    private String helper(int num) {
-        String res = "";
-        if (num < 20) {
-            res = LESS_THAN_TWENTY[num];
-        } else if (num < HUNDRED) {
-            res = TENS[num / 10] + " " + helper(num % 10);
-        } else if (num < THOUSAND) {
-            res = helper(num / HUNDRED) + " Hundred " + helper(num % HUNDRED);
-        } else if (num < MILLION) {
-            res = helper(num / THOUSAND) + " Thousand " + helper(num % THOUSAND);
-        } else if (num < BILLION) {
-            res = helper(num / MILLION) + " Million " + helper(num % MILLION);
-        } else {
-            res = helper(num / BILLION) + " Billion " + helper(num % BILLION);
+        if(num==0){
+            return "Zero";
         }
-        return res.trim();
+        int level = 0;
+        String res = "";
+        while(num!=0){
+            int three = num % 1000;
+            if(three!=0){
+                StringBuilder sb = new StringBuilder();
+                sb.append(threeDigits(three)).append(levels[level]);
+                if(res.length()>0){
+                    sb.append(" ").append(res);
+                }
+                res = sb.toString();
+            }
+            num = num/1000;
+            level++;
+        }
+        return res;
+    }
+    public String threeDigits(int num) {
+        if(num<20){
+            return lessThan20[num];
+        }
+        if(num<100){
+            int last = num%10;
+            if(last==0){
+                return tens[num/10];
+            }
+            return  tens[num/10] + " " + lessThan20[last];
+        }
+        int hundred = num /100;
+        int rest = num % 100;
+        if(rest!=0){
+            return lessThan20[hundred] + " Hundred "+ threeDigits(rest);
+        }
+        return lessThan20[hundred] + " Hundred";
     }
 }
