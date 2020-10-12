40b9a915a228beeed7954276ebdba1e30c439803
CityChan <53466807+CityChan@users.noreply.github.com>
2020-10-12 02:51:12 +0000

    [LeetCode Sync] Runtime - 2 ms (23.92%), Memory - 38.8 MB (100.00%)

--- my-folder/0065-valid-number/solution.java
+++ my-folder/0065-valid-number/solution.java
@@ -1,40 +1,37 @@
 class Solution {
     public boolean isNumber(String s) {
-        char[] str = s.trim().toCharArray();
-        // int start = 0;
-        // if(str[0]=='+'||str[0]=='-'){
-        //     ++start;
-        // }
-        
-        boolean hasDigitBeforeExponent = false;
-        boolean exponent = false;
-        boolean hasDigitAfterExponent = false;
-        boolean hasDecimal = false;
-        for(int i=0;i<str.length;++i){
-            if(Character.isDigit(str[i])){
-                if(exponent){
-                    hasDigitAfterExponent = true;
-                }else{
-                    hasDigitBeforeExponent = true;
-                }
-            }else if(str[i]=='.'){
-                if(exponent||hasDecimal){
-                    return false;
-                }
-                hasDecimal = true;
-            }else if(str[i]=='e'){
-                if(exponent||!hasDigitBeforeExponent){
-                    return false;
-                }
-                exponent = true;
-            }else if(str[i]=='+'||str[i]=='-'){
-                if(i!=0&&str[i-1]!='e'){
-                    return false;
-                }
-            }else{
-                return false;
-            }
+        if (s == null || s.length() == 0) {
+      return false;
+    }
+    s = s.trim();
+    int n = s.length();
+    boolean hasDigit = false;
+    boolean hasPoint = false;
+    boolean hasE = false;
+    for (int i = 0; i < n; i++) {
+      char c = s.charAt(i);
+      if (c >= '0' && c <= '9') { // is digit
+        hasDigit = true;
+      } else if (c == '.') {
+        if (hasPoint || hasE) {
+          return false;
         }
-        return exponent? hasDigitAfterExponent: hasDigitBeforeExponent;
+        hasPoint = true;
+        // hasDigit = false;
+      } else if (c == '-' || c == '+') {
+        if (i != 0 && s.charAt(i - 1) != 'e') {
+          return false;
+        }
+      } else if (c == 'e') {
+        if(hasE || !hasDigit){
+          return false;
+        }
+        hasE = true;
+        hasDigit = false;
+      } else { // other characters
+        return false;
+      }
+    }
+    return hasDigit;
     }
 }
