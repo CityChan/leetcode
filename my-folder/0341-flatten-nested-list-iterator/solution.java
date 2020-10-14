33b7eb42736b7c16b3ce921d7a64b2d986405c29
CityChan <53466807+CityChan@users.noreply.github.com>
2020-10-14 08:41:04 +0000

    [LeetCode Sync] Runtime - 3 ms (57.86%), Memory - 41.7 MB (100.00%)

--- my-folder/0341-flatten-nested-list-iterator/solution.java
+++ my-folder/0341-flatten-nested-list-iterator/solution.java
@@ -16,31 +16,31 @@
  * }
  */
 public class NestedIterator implements Iterator<Integer> {
-    Deque<NestedInteger> stack = new ArrayDeque<>();
+    Deque<NestedInteger> stack;
 
     public NestedIterator(List<NestedInteger> nestedList) {
-        flatten(nestedList);
+        stack  = new ArrayDeque<>(nestedList);
     }
 
     @Override
     public Integer next() {
-        if(hasNext()){
-            return stack.pop().getInteger();
-        }
-        return null;
+        if (!hasNext()) 
+            return null;
+        return stack.pop().getInteger();
     }
 
     @Override
     public boolean hasNext() {
-        while(!stack.isEmpty()&&!stack.peek().isInteger()){
-            flatten(stack.pop().getList());
-        }
+        flatten();
         return !stack.isEmpty();
     }
     
-    private void flatten(List<NestedInteger> list){
-        for(int i=list.size()-1;i>=0;--i){
-            stack.push(list.get(i));
+    private void flatten(){
+        while(!stack.isEmpty()&&!stack.peek().isInteger()){
+            List<NestedInteger> nested = stack.pop().getList();
+            for(int i=nested.size()-1;i>=0;i--){
+                stack.push(nested.get(i));
+            }
         }
     }
 }
