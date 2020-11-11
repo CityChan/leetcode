9f6875f7952334ca3dbc065c1211a8985388ca12
CityChan <53466807+CityChan@users.noreply.github.com>
2020-11-11 04:44:13 +0000

    [LeetCode Sync] Runtime - 20 ms (0.00%), Memory - 14.1 MB (100.00%)

---
+++ my-folder/0163-missing-ranges/solution.py
@@ -0,0 +1,15 @@
+class Solution:
+    def findMissingRanges(self, nums: List[int], lower: int, upper: int) -> List[str]:
+        nums = [lower-1] + nums + [upper+1]
+        res = []
+
+        for i in range(len(nums) - 1):
+            if nums[i+1] - nums[i] == 2:
+                # plus one to the lower number
+                res.append(str(nums[i]+1))
+            elif nums[i+1] - nums[i] > 2:
+                # plus one of nums[i] as the lower
+                # minus one of nums[i+1] as the upper
+                res.append(str(nums[i]+1) + '->' + str(nums[i+1]-1))
+        return res
+
