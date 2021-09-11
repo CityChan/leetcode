15439e09cba0214b607877dbad67fd2392d4b415
CityChan <53466807+CityChan@users.noreply.github.com>
2021-09-11 04:50:21 +0000

    [LeetCode Sync] Runtime - 116 ms (31.22%), Memory - 15.2 MB (100.00%)

--- my-folder/0045-jump-game-ii/solution.py
+++ my-folder/0045-jump-game-ii/solution.py
@@ -1,14 +1,15 @@
 class Solution:
     def jump(self, nums: List[int]) -> int:
-        count = 0
-        res = [[0]]
-        while len(nums)-1 not in res[-1]:
-            temp_res = set()
-            for i in res[-1]:
-                for j in range(1, nums[i]+1):
-                    temp_res.add(i+j)
-            res+=[temp_res]
-            count+=1
-        return count
-                    
+        if len(nums)==1:
+            return 0
+        count=0
+        now_pos = 0
+        far_range = 0
+        for i in range(len(nums)):
+            far_range = max(far_range, nums[i]+i)
+            if i==now_pos:
+                count+=1
+                now_pos = far_range
+                if now_pos>=len(nums)-1:
+                    return count
             
