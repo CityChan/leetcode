275776f686ca157704e47e550d25ff06002aee93
CityChan <53466807+CityChan@users.noreply.github.com>
2021-12-12 03:42:50 +0000

    [LeetCode Sync] Runtime - 55 ms (24.06%), Memory - 13.7 MB (6.17%)

--- my-folder/0003-longest-substring-without-repeating-characters/solution.py
+++ my-folder/0003-longest-substring-without-repeating-characters/solution.py
@@ -1,21 +1,23 @@
-class Solution:
-    def lengthOfLongestSubstring(self, s: str) -> int:
-        if len(s)==0:
-            return 0
-        res=s[0]
-        temp_res=s[0]
-        left, right = 0, 1
-        while right<len(s):
-            temp = temp_res.find(s[right])
-            if temp==-1:
-                temp_res+=s[right]
-            else:
-                left=right-len(temp_res)+temp+1
-                if len(temp_res)>len(res):
-                    res=temp_res
-                temp_res=s[left:right+1]
-            right+=1
-        return max(len(res), len(temp_res))
-                
-                
+class Solution(object):
+    def lengthOfLongestSubstring(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        if len(s)<2:
+            return len(s)
+        save_dict = {s[0]: 0}
+        start, end = 0, 1
+        max_len = 1
+        while end<len(s):
+            if (s[end] in save_dict) and (save_dict[s[end]]>=start):
+                start = save_dict[s[end]] + 1
+                           
+            save_dict[s[end]] = end
+            max_len = max(max_len, end-start+1)
+            
+            end+=1
+            
+        return max_len
+            
         
