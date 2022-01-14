43db02a36af841a920a59e330c071f3ed033ebba
CityChan <53466807+CityChan@users.noreply.github.com>
2022-01-14 21:40:23 +0000

    [LeetCode Sync] Runtime - 30 ms (6.33%), Memory - 13.2 MB (5.77%)

--- my-folder/0088-merge-sorted-array/solution.py
+++ my-folder/0088-merge-sorted-array/solution.py
@@ -1,20 +1,27 @@
-class Solution:
-    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+class Solution(object):
+    def merge(self, nums1, m, nums2, n):
         """
-        Do not return anything, modify nums1 in-place instead.
+        :type nums1: List[int]
+        :type m: int
+        :type nums2: List[int]
+        :type n: int
+        :rtype: None Do not return anything, modify nums1 in-place instead.
         """
-        index = m + n - 1
-        m = m - 1
-        n = n - 1
+        now_idx = m+n-1
+        idx1 = m-1
+        idx2 = n-1
         
-        while n >= 0 and m >= 0:
-            if nums1[m] > nums2[n]:
-                nums1[index] = nums1[m]
-                m -= 1
+        while idx1>=0 and idx2>=0:
+            if nums1[idx1]>=nums2[idx2]:
+                nums1[now_idx] = nums1[idx1]
+                idx1-=1
             else:
-                nums1[index] = nums2[n]
-                n -= 1
-            index -= 1
+                nums1[now_idx] = nums2[idx2]
+                idx2-=1
+            now_idx-=1
+            
+        if idx2 >= 0:
+            nums1[:idx2+1] = nums2[:idx2+1]
+        return nums1
+                
         
-        if n >= 0:
-            nums1[0: index+1] = nums2[0: n+1]
