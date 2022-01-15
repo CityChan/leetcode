9e6cab3c411d94ea7844026c077645e937faf8e9
CityChan <53466807+CityChan@users.noreply.github.com>
2022-01-15 19:47:07 +0000

    [LeetCode Sync] Runtime - 1576 ms (5.06%), Memory - 22.7 MB (7.31%)

--- my-folder/0121-best-time-to-buy-and-sell-stock/solution.py
+++ my-folder/0121-best-time-to-buy-and-sell-stock/solution.py
@@ -4,39 +4,11 @@ class Solution(object):
         :type prices: List[int]
         :rtype: int
         """
-#         subprices = [prices[i+1]-prices[i] for i in range(len(prices)-1)]
-#         i=0
-#         while i<len(subprices) and subprices[i]<0:
-#             i+=1
-            
-#         s = 0
-#         s_max = 0
-#         for j in range(i, len(subprices)):
-#             s+=subprices[j]
-#             s_max = max(s, s_max)
-#             if s<0:
-#                 s=0
-#         return s_max
+        minCost = prices[0]
+        maxProfit = 0
+        for p in prices[1:]:
+            maxProfit = max(maxProfit, p-minCost)
+            minCost = min(minCost, p)
+        return maxProfit
                 
-        if len(prices)==1:
-            return 0
-        i, j = 0, 1
-        while j<len(prices) and prices[j]-prices[i]<=0:
-            i+=1
-            j+=1
-        if j==len(prices):
-            return 0
-        
-        s_max = 0
-        
-        while j<len(prices):
-            s_max = max(s_max, prices[j]-prices[i])
-            if prices[j]-prices[i]<=0:
-                i=j
-                j=i+1
-            else:
-                j+=1
-        return s_max
-        
-        
         
