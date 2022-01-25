6705c9f8feca78c8e7c97af78a0aa6eb3b38cec3
CityChan <53466807+CityChan@users.noreply.github.com>
2022-01-25 07:34:38 +0000

    [LeetCode Sync] Runtime - 903 ms (38.75%), Memory - 13.7 MB (17.92%)

--- my-folder/0005-longest-palindromic-substring/solution.py
+++ my-folder/0005-longest-palindromic-substring/solution.py
@@ -1,24 +1,29 @@
-class Solution:
-    def longestPalindrome(self, s: str) -> str:
+class Solution(object):
+    def longestPalindrome(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
         def helper(s, i, j):
-            while i>=0 and j<=len(s)-1:
-                if s[i]==s[j]:
-                    i-=1
-                    j+=1
-                else:
-                    break
-            return s[i+1:j]
-        res=""
-        for i in range(len(s)):
-            res1=helper(s, i, i)
-            if len(res1)>len(res):
-                res = res1
-            if i<len(s)-1 and s[i]==s[i+1]:
-                res1=helper(s, i, i+1)
-                if len(res1)>len(res):
-                    res = res1
-        return res
-            
+            while i>=0 and j<len(s) and s[i]==s[j]:
+                i-=1
+                j+=1
+            return i+1, j-1
+        
+        max_len = 0
+        max_str = ""
+        for n in range(len(s)):
+            i, j = helper(s, n, n)
+            if j-i+1>max_len:
+                max_len = j-i+1
+                max_str = s[i:j+1]
+            if (n+1)<len(s) and s[n]==s[n+1]:
+                i, j = helper(s, n, n+1)
+                if j-i+1>max_len:
+                    max_len = j-i+1
+                    max_str = s[i:j+1]
+        return max_str
+                
                 
             
         
