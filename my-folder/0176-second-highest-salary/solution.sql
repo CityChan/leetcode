0c2ac4bd7caf17cc369d4bf56dd24b12ea8c4551
CityChan <53466807+CityChan@users.noreply.github.com>
2022-01-27 03:56:11 +0000

    [LeetCode Sync] Runtime - 247 ms (96.59%), Memory - 0B (100.00%)

--- my-folder/0176-second-highest-salary/solution.sql
+++ my-folder/0176-second-highest-salary/solution.sql
@@ -1,3 +1,7 @@
 # Write your MySQL query statement below
-select max(salary) as SecondHighestSalary 
-from Employee where salary <> (select max(salary) from Employee)
+SELECT max(salary) AS SecondHighestSalary
+From Employee
+Where salary < (SELECT max(salary) FROM Employee)
+# SELECT DISTINCT e1.salary AS SecondHighestSalary
+# FROM Employee e1
+# WHERE 1= (SELECT COUNT(DISTINCT e2.salary) FROM Employee e2 WHERE e1.salary<e2.salary)
