41a2a4522eb2a505640394dff8b1a5797f1651ca
CityChan <53466807+CityChan@users.noreply.github.com>
2022-01-27 04:08:45 +0000

    [LeetCode Sync] Runtime - 552 ms (47.66%), Memory - 0B (100.00%)

--- my-folder/0181-employees-earning-more-than-their-managers/solution.sql
+++ my-folder/0181-employees-earning-more-than-their-managers/solution.sql
@@ -1,4 +1,4 @@
-/* Write your T-SQL query statement below */
+# Write your MySQL query statement below
 SELECT e1.name Employee
 FROM Employee e1, Employee e2
-Where e1.managerId = e2.id and e1.salary > e2.salary
+WHERE e1.managerId=e2.id and e1.salary>e2.salary
