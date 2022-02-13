82dc79e78424fc68ec8dccc3724935026d9109ec
CityChan <53466807+CityChan@users.noreply.github.com>
2022-02-13 21:22:57 +0000

    [LeetCode Sync] Runtime - 823 ms (39.36%), Memory - 0B (100.00%)

--- my-folder/2158-the-number-of-seniors-and-juniors-to-join-the-company-ii/solution.sql
+++ my-folder/2158-the-number-of-seniors-and-juniors-to-join-the-company-ii/solution.sql
@@ -1,9 +1,9 @@
 # Write your MySQL query statement below
-WITH TEMP AS (SELECT employee_id, experience, SUM(salary) OVER (PARTITION BY experience ORDER BY salary) "cumsalary" FROM CANDIDATES),
-TEMP1 AS
-(SELECT * FROM TEMP WHERE experience="Senior" AND cumsalary<=70000)
+WITH TEMP AS (SELECT employee_id, experience, SUM(salary) OVER (PARTITION BY experience ORDER BY salary) "cumsalary" FROM CANDIDATES)
+#  ,TEMP1 AS
+# (SELECT * FROM TEMP WHERE experience="Senior" AND cumsalary<=70000)
 
-SELECT employee_id FROM TEMP1
+(SELECT employee_id FROM TEMP WHERE experience="Senior" AND cumsalary<=70000)
 UNION ALL
 SELECT employee_id FROM TEMP WHERE experience="Junior" 
-AND cumsalary<(SELECT 70000-IFNULL(MAX(cumsalary),0) FROM TEMP1)
+AND cumsalary<(SELECT 70000-IFNULL(MAX(cumsalary),0) FROM TEMP WHERE experience="Senior" AND cumsalary<=70000)
