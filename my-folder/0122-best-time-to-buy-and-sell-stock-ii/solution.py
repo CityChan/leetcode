1d38dcce1f86339fc78924ee5e2d76428e9db12f
CityChan <53466807+CityChan@users.noreply.github.com>
2022-04-20 16:37:14 +0000

    [LeetCode Sync] Runtime - 55 ms (5.24%), Memory - 14.5 MB (8.09%)

--- my-folder/0122-best-time-to-buy-and-sell-stock-ii/solution.py
+++ my-folder/0122-best-time-to-buy-and-sell-stock-ii/solution.py
@@ -1,7 +1,8 @@
-class Solution:
-    def maxProfit(self, prices: List[int]) -> int:
-        maxProfit = 0
-        for i in range(1, len(prices)):
-            if prices[i] > prices[i-1]:
-                maxProfit += prices[i] - prices[i-1]
-        return maxProfit
+class Solution(object):
+    def maxProfit(self, prices):
+        """
+        :type prices: List[int]
+        :rtype: int
+        """
+        return sum([prices[i]-prices[i-1] for i in range(1, len(prices)) if prices[i]>prices[i-1]])
+        
