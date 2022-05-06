486a077037cace0a95c9ae9349f6871a22927f21
CityChan <53466807+CityChan@users.noreply.github.com>
2022-05-06 07:43:34 +0000

    [LeetCode Sync] Runtime - 22 ms (2.28%), Memory - 13.5 MB (21.07%)

--- my-folder/0017-letter-combinations-of-a-phone-number/solution.py
+++ my-folder/0017-letter-combinations-of-a-phone-number/solution.py
@@ -4,22 +4,16 @@ class Solution(object):
         :type digits: str
         :rtype: List[str]
         """
-        letter_dict = {2: "abc", 3: "def", 4: "ghi", 5: "jkl", 6: "mno",
-                      7: "pqrs", 8: "tuv", 9: "wxyz"}
+        letter_dict = {"2": "abc", "3": "def", "4": "ghi", "5": "jkl", "6": "mno",
+                      "7": "pqrs", "8": "tuv", "9": "wxyz"}
         def recur(digits):
             if len(digits)==1:
-                return list(letter_dict[int(digits)])
-            if len(digits)==2:
-                res = []
-                for i in letter_dict[int(digits[0])]:
-                    for j in letter_dict[int(digits[1])]:
-                        res.append(i+j)
-                return  res
-            if len(digits)>2:
+                return list(letter_dict[digits])
+            elif len(digits)>=2:
                 res = []
                 left_res = recur(digits[:-1])
                 for i in left_res:
-                    for j in letter_dict[int(digits[-1])]:
+                    for j in letter_dict[digits[-1]]:
                         res.append(i+j)
                 return res
         return recur(digits)
