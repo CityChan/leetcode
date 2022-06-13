38851faa066be14c1cd81402a6ac55644217ddeb
CityChan <53466807+CityChan@users.noreply.github.com>
2022-06-13 04:41:33 +0000

    [LeetCode Sync] Runtime - 21 ms (3.35%), Memory - 13.4 MB (5.45%)

--- my-folder/0198-house-robber/solution.py
+++ my-folder/0198-house-robber/solution.py
@@ -4,13 +4,9 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        if len(nums)==1:
-            return nums[0]
-        
-        res = [0]*len(nums)
-        res[0] = nums[0]
-        res[1] = max(nums[0], nums[1])
-        for i in range(2, len(nums)):
-            res[i] = max(res[i-1], res[i-2]+nums[i])
-        return max(res[-1], res[-2])
+        pre = 0
+        now = nums[0]
+        for i in range(1, len(nums)):
+            now, pre = max(pre+nums[i], now), now
+        return max(pre, now)
         
