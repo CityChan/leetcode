fa944d480975b6952af11d7c913bbad1eccbf385
CityChan <53466807+CityChan@users.noreply.github.com>
2022-07-17 21:46:48 +0000

    [LeetCode Sync] Runtime - 102 ms (25.16%), Memory - 14.2 MB (5.14%)

--- my-folder/0045-jump-game-ii/solution.py
+++ my-folder/0045-jump-game-ii/solution.py
@@ -4,18 +4,34 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
+        # step = 0
+        # farthest = 0
+        # now = 0
+        # if len(nums)==1:
+        #     return 0
+        # for i, n in enumerate(nums):
+        #     farthest = max(farthest, i+n)
+        #     if i==now:
+        #         step+=1
+        #         now = farthest
+        #         if len(nums)-1<=farthest:
+        #             return step
+        
+        now_max = 0
         step = 0
         farthest = 0
-        now = 0
+        i = 0
         if len(nums)==1:
             return 0
-        for i, n in enumerate(nums):
-            farthest = max(farthest, i+n)
-            if i==now:
-                step+=1
-                now = farthest
-                if len(nums)-1<=farthest:
-                    return step
+        while 1:
+            farthest = max(farthest, i+nums[i])
+            if farthest>=len(nums)-1:
+                return step+1
+            if i == now_max:
+                step += 1
+                now_max = farthest
+            i+=1
+        
                     
                     
         
