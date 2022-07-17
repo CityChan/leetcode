b61fc295437aa33697237831c9da97c9c8c9b5ec
CityChan <53466807+CityChan@users.noreply.github.com>
2022-07-17 06:02:13 +0000

    [LeetCode Sync] Runtime - 1187 ms (5.18%), Memory - 29.9 MB (5.19%)

--- my-folder/1909-buildings-with-an-ocean-view/solution.py
+++ my-folder/1909-buildings-with-an-ocean-view/solution.py
@@ -4,12 +4,11 @@ class Solution(object):
         :type heights: List[int]
         :rtype: List[int]
         """
-        max_h = 0
         res = []
-        for i in range(len(heights)):
-            if heights[len(heights)-1-i]>max_h:
-                res+=[len(heights)-1-i]
-                max_h = heights[len(heights)-1-i]
-        return res[::-1]
-                
+        highest = 0
+        for i, h in enumerate(heights[::-1]):
+            if h>highest:
+                res.append(i)
+            highest = max(highest, h)
+        return [len(heights)-1-i for i in res[::-1]]
         
