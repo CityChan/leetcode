ce374f00a7d431961bb30944fde33a5fec54429c
CityChan <53466807+CityChan@users.noreply.github.com>
2022-07-20 04:36:32 +0000

    [LeetCode Sync] Runtime - 611 ms (59.18%), Memory - 0B (100.00%)

--- my-folder/1623-sales-by-day-of-the-week/solution.sql
+++ my-folder/1623-sales-by-day-of-the-week/solution.sql
@@ -1,12 +1,12 @@
 # Write your MySQL query statement below
 SELECT item_category Category,
-SUM(CASE WHEN DAYOFWEEK(order_date)=2 THEN quantity ELSE 0 END) 'Monday',
-SUM(CASE WHEN DAYOFWEEK(order_date)=3 THEN quantity ELSE 0 END) 'Tuesday',
-SUM(CASE WHEN DAYOFWEEK(order_date)=4 THEN quantity ELSE 0 END) 'Wednesday',
-SUM(CASE WHEN DAYOFWEEK(order_date)=5 THEN quantity ELSE 0 END) 'Thursday',
-SUM(CASE WHEN DAYOFWEEK(order_date)=6 THEN quantity ELSE 0 END) 'Friday',
-SUM(CASE WHEN DAYOFWEEK(order_date)=7 THEN quantity ELSE 0 END) 'Saturday',
-SUM(CASE WHEN DAYOFWEEK(order_date)=1 THEN quantity ELSE 0 END) 'Sunday'
+SUM(CASE WHEN WEEKDAY(order_date)=0 THEN quantity ELSE 0 END) 'Monday',
+SUM(CASE WHEN WEEKDAY(order_date)=1 THEN quantity ELSE 0 END) 'Tuesday',
+SUM(CASE WHEN WEEKDAY(order_date)=2 THEN quantity ELSE 0 END) 'Wednesday',
+SUM(CASE WHEN WEEKDAY(order_date)=3 THEN quantity ELSE 0 END) 'Thursday',
+SUM(CASE WHEN WEEKDAY(order_date)=4 THEN quantity ELSE 0 END) 'Friday',
+SUM(CASE WHEN WEEKDAY(order_date)=5 THEN quantity ELSE 0 END) 'Saturday',
+SUM(CASE WHEN WEEKDAY(order_date)=6 THEN quantity ELSE 0 END) 'Sunday'
 FROM Orders O1 RIGHT JOIN Items I1
 ON O1.item_id = I1.item_id
 GROUP BY item_category
