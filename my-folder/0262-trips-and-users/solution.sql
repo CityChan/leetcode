939d9c9d66b56297487a0215fee9fb7aa3de4991
CityChan <53466807+CityChan@users.noreply.github.com>
2022-07-20 04:30:22 +0000

    [LeetCode Sync] Runtime - 1574 ms (5.73%), Memory - 0B (100.00%)

--- my-folder/0262-trips-and-users/solution.sql
+++ my-folder/0262-trips-and-users/solution.sql
@@ -6,7 +6,9 @@
 #       AND Client_id NOT IN (SELECT Users_Id FROM Users WHERE Banned = 'Yes')
 # GROUP BY Request_at;
 
-SELECT request_at day, ROUND(SUM(IF(Status !='completed', 1, 0))/count(*),2) AS "cancellation rate" FROM
+SELECT request_at day, ROUND(COUNT(IF(Status !='completed', 1, NULL))/count(*),2) AS "cancellation rate" FROM
 (SELECT * FROM Trips WHERE client_id IN (SELECT users_id FROM Users WHERE banned="No" and role = "client")
 and driver_id IN (SELECT users_id FROM Users WHERE banned="No" and role = "driver")) T1
 Where request_At BETWEEN '2013-10-01' AND '2013-10-03' GROUP BY REQUEST_AT
+
+
