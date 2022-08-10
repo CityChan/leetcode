be382790459ed14b6f6187ffb62399c29c1ea2d7
CityChan <53466807+CityChan@users.noreply.github.com>
2022-08-10 07:27:23 +0000

    [LeetCode Sync] Runtime - 351 ms (64.26%), Memory - 0B (100.00%)

--- my-folder/0178-rank-scores/solution.sql
+++ my-folder/0178-rank-scores/solution.sql
@@ -1,7 +1,9 @@
 # Write your MySQL query statement below
-SELECT s3.score, s2.rank from SCORES s3,
-(SELECT score, row_number() OVER (ORDER BY score DESC) "rank"
-FROM
-(SELECT DISTINCT score from SCORES) s1) s2
-WHERE s3.score=s2.score
-order by s3.score DESC
+# SELECT s3.score, s2.rank from SCORES s3,
+# (SELECT score, row_number() OVER (ORDER BY score DESC) "rank"
+# FROM
+# (SELECT DISTINCT score from SCORES) s1) s2
+# WHERE s3.score=s2.score
+# order by s3.score DESC
+SELECT score, dense_rank() OVER(ORDER BY score DESC) AS "rank"
+FROM Scores
