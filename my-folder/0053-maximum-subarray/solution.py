f48ee672b9387262092ee155f8b3378fa87782c6
CityChan <53466807+CityChan@users.noreply.github.com>
2022-08-10 00:57:50 +0000

    [LeetCode Sync] Runtime - 858 ms (5.05%), Memory - 25.7 MB (5.37%)

--- my-folder/0053-maximum-subarray/solution.py
+++ my-folder/0053-maximum-subarray/solution.py
@@ -5,17 +5,17 @@ class Solution(object):
         :rtype: int
         """
         
-        # now_subsum=nums[0]
-        # max_subsum=nums[0]
-        # for i in nums[1:]:
-        #     now_subsum = max(now_subsum+i, i)
-        #     max_subsum = max(now_subsum, max_subsum)
-        # return max_subsum
-        min_sum = 0
-        max_sum = nums[0]-10001
-        now_sum = 0
-        for i in nums:
-            now_sum+=i
-            max_sum = max(max_sum, now_sum-min_sum)
-            min_sum = min(min_sum, now_sum)
-        return max_sum        
+        now_subsum=nums[0]
+        max_subsum=nums[0]
+        for i in nums[1:]:
+            now_subsum = max(now_subsum+i, i)
+            max_subsum = max(now_subsum, max_subsum)
+        return max_subsum
+        # min_sum = 0
+        # max_sum = nums[0]-10001
+        # now_sum = 0
+        # for i in nums:
+        #     now_sum+=i
+        #     max_sum = max(max_sum, now_sum-min_sum)
+        #     min_sum = min(min_sum, now_sum)
+        # return max_sum        
