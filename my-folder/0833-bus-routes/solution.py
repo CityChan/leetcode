1a8afc7bd606492cc5df177e2dfe5f48be176f97
CityChan <53466807+CityChan@users.noreply.github.com>
2022-08-14 07:00:41 +0000

    [LeetCode Sync] Runtime - 1278 ms (28.66%), Memory - 51.9 MB (5.73%)

--- my-folder/0833-bus-routes/solution.py
+++ my-folder/0833-bus-routes/solution.py
@@ -6,14 +6,18 @@ class Solution(object):
                 to_routes[j].add(i)
         bfs = [(S, 0)]
         seen = set([S])
+        seen_routes = set([])
         while bfs:
             stop, bus = bfs.pop(0)
             if stop == T: return bus
             for i in to_routes[stop]:
-                while routes[i]:
-                    j = routes[i].pop()
-                    if j not in seen:
-                        bfs.append((j, bus + 1))
-                        seen.add(j)
+                if i not in seen_routes:
+                    for j in routes[i]:
+                        if j not in seen:
+                            if j==T:
+                                return bus+1
+                            bfs.append((j, bus + 1))
+                            seen.add(j)
+                    seen_routes.add(i)
                 #routes[i] = []  # seen route
         return -1
