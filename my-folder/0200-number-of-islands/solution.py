cd2b3d06ccffcafb80958b22bb7973886ac52156
CityChan <53466807+CityChan@users.noreply.github.com>
2022-08-20 01:11:53 +0000

    [LeetCode Sync] Runtime - 298 ms (18.24%), Memory - 28.5 MB (41.01%)

--- my-folder/0200-number-of-islands/solution.py
+++ my-folder/0200-number-of-islands/solution.py
@@ -4,18 +4,40 @@ class Solution(object):
         :type grid: List[List[str]]
         :rtype: int
         """
+        # count = 0
+        # def search_p(grid, i, j):
+        #     if i>=0 and i<len(grid) and j>=0 and j<len(grid[0]) and grid[i][j] == '1':
+        #             grid[i][j] = '0'
+        #             for ii, jj in ((0, 1), (0, -1), (1, 0), (-1, 0)):
+        #                 search_p(grid, i+ii, j+jj)
+        # for i in range(len(grid)):
+        #     for j in range(len(grid[0])):
+        #         if grid[i][j] == '1':
+        #             count +=1
+        #             search_p(grid, i, j)
+        # return count
         count = 0
-        def search_p(grid, i, j):
-            if i>=0 and i<len(grid) and j>=0 and j<len(grid[0]) and grid[i][j] == '1':
-                    grid[i][j] = '0'
-                    for ii, jj in ((0, 1), (0, -1), (1, 0), (-1, 0)):
-                        search_p(grid, i+ii, j+jj)
         for i in range(len(grid)):
             for j in range(len(grid[0])):
-                if grid[i][j] == '1':
-                    count +=1
-                    search_p(grid, i, j)
+                if grid[i][j]=='1':
+                    count+=1
+                    grid[i][j]='0'
+                    q = [(i, j)]
+                    while q:
+
+                        ii, jj = q.pop(0)
+                        for di, dj in ((-1, 0), (1, 0), (0, 1), (0, -1)):
+       
+                            nxt_i = ii+di
+                            nxt_j = jj+dj
+         
+                            if 0<=nxt_i<len(grid) and 0<=nxt_j<len(grid[0]) and grid[nxt_i][nxt_j]=='1':
+        
+                                grid[nxt_i][nxt_j]='0'
+                                q.append((nxt_i, nxt_j))
+      
         return count
+                
             
         
         
@@ -42,31 +64,6 @@ class Solution(object):
         
         
         
-        
-        
-        
-        
-        
-        
-        
-        
-        
-#         def search_p(grid, i, j, m, n):
-#             grid[i][j]="2"
-#             for aa, bb in ((-1, 0), (1, 0), (0, 1), (0, -1)):
-#                 ii, jj = i+aa, j+bb
-#                 if ii>=0 and ii<m and jj>=0 and jj<n and grid[ii][jj]=="1":
-#                     search_p(grid, ii, jj, m, n)
-                    
-#         c = 0
-#         m = len(grid)
-#         n = len(grid[0])
-#         for i in range(m):
-#             for j in range(n):
-#                 if grid[i][j]=="1":
-#                     search_p(grid, i, j, m, n)
-#                     c+=1
-#         return c
         
         
         
