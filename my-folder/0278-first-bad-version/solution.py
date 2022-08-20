8eef28d5aff0aac6bfa803bb0dd0911060cbb75d
CityChan <53466807+CityChan@users.noreply.github.com>
2022-08-20 04:00:52 +0000

    [LeetCode Sync] Runtime - 32 ms (7.11%), Memory - 13.3 MB (23.14%)

--- my-folder/0278-first-bad-version/solution.py
+++ my-folder/0278-first-bad-version/solution.py
@@ -11,10 +11,10 @@ class Solution(object):
         """
         start = 1
         end = n
-        while start<end:
+        while start<=end:
             mid = start+(end-start)//2
             if isBadVersion(mid):
-                end = mid
+                end = mid-1
             else:
                 start = mid+1
         return start
