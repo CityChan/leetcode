27f1d47f902946afaa74be91c29eca353c521e2e
CityChan <53466807+CityChan@users.noreply.github.com>
2022-08-20 03:24:06 +0000

    [LeetCode Sync] Runtime - 148 ms (5.70%), Memory - 13.7 MB (71.78%)

--- my-folder/0695-max-area-of-island/solution.py
+++ my-folder/0695-max-area-of-island/solution.py
@@ -4,25 +4,28 @@ class Solution(object):
         :type grid: List[List[int]]
         :rtype: int
         """
-        def dfs(grid, i, j):
-            if i<0 or i>=len(grid) or j<0 or j>=len(grid[0]) or grid[i][j]!=1:
-                return 0
-            else:
-                area[0]+=1
-                grid[i][j]=0
-                dfs(grid, i-1, j)
-                dfs(grid, i+1, j)
-                dfs(grid, i, j-1)
-                dfs(grid, i, j+1)
-            # return now_count+1
-                
+        # count = 0
         max_area = 0
         for i in range(len(grid)):
             for j in range(len(grid[0])):
-                area=[0]
-                if grid[i][j]==1:    
-                    dfs(grid, i, j)
-                    max_area = max(max_area, area[0])
+                if grid[i][j]==1:
+                    area = 0
+                    # count+=1
+                    grid[i][j]=0
+                    q = [(i, j)]
+                    while q:
+                        ii, jj = q.pop(0)
+                        area+=1
+                        for di, dj in ((-1, 0), (1, 0), (0, 1), (0, -1)):
+       
+                            nxt_i = ii+di
+                            nxt_j = jj+dj
+         
+                            if 0<=nxt_i<len(grid) and 0<=nxt_j<len(grid[0]) and grid[nxt_i][nxt_j]==1:
+                                # area+=1
+                                grid[nxt_i][nxt_j]=0
+                                q.append((nxt_i, nxt_j))
+                    max_area = max(max_area, area)
         
         
         return max_area
