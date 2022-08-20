528ad1c99ed0698140ce23e637385c5f1066ba84
CityChan <53466807+CityChan@users.noreply.github.com>
2022-08-20 04:41:12 +0000

    [LeetCode Sync] Runtime - 66 ms (6.59%), Memory - 13.6 MB (8.86%)

--- my-folder/0733-flood-fill/solution.py
+++ my-folder/0733-flood-fill/solution.py
@@ -9,14 +9,21 @@ class Solution(object):
         """
         m = len(image)
         n = len(image[0])
+        
         oldColor = image[sr][sc]
+        
         if oldColor == newColor:
             return image
-        def dfs(image, r, c):
-            if 0<=r<m and 0<=c<n and image[r][c]==oldColor:
-                image[r][c]=newColor
-                for d in ((1, 0), (-1, 0), (0, 1), (0, -1)):
-                    dfs(image, r+d[0], c+d[1])
-        dfs(image, sr, sc)
+        
+        q = [(sr, sc)]
+        image[sr][sc]=newColor
+        while q:
+            r, c = q.pop(0)
+            # image[r][c]=newColor
+            for d in ((1, 0), (-1, 0), (0, 1), (0, -1)):
+                nxt_r, nxt_c = r+d[0], c+d[1]
+                if 0<=nxt_r<m and 0<=nxt_c<n and image[nxt_r][nxt_c]==oldColor:
+                    image[nxt_r][nxt_c]=newColor
+                    q.append((nxt_r, nxt_c))
         return image
         
