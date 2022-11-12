2474192e21de890d934aa54d4cc8c35b991e9f7e
CityChan <53466807+CityChan@users.noreply.github.com>
2022-11-12 22:14:14 +0000

    [LeetCode Sync] Runtime - 506 ms (93.53%), Memory - 0B (100.00%)

--- my-folder/0183-customers-who-never-order/solution.sql
+++ my-folder/0183-customers-who-never-order/solution.sql
@@ -1,6 +1,10 @@
 # Write your MySQL query statement below
-SELECT c1.name Customers
-FROM Customers c1
-LEFT JOIN Orders o1
-on c1.id=o1.customerId
-Where ISNULL(o1.customerId)
+# SELECT c1.name Customers
+# FROM Customers c1
+# LEFT JOIN Orders o1
+# on c1.id=o1.customerId
+# Where ISNULL(o1.customerId)
+
+SELECT name Customers
+FROM Customers
+WHERE id not in (SELECT DISTINCT customerId FROM Orders)
