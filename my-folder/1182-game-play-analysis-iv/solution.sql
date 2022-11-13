a718c99b734c9485ce6d39cfe15149841c489a48
CityChan <53466807+CityChan@users.noreply.github.com>
2022-11-13 23:06:53 +0000

    [LeetCode Sync] Runtime - 672 ms (62.80%), Memory - 0B (100.00%)

--- my-folder/1182-game-play-analysis-iv/solution.sql
+++ my-folder/1182-game-play-analysis-iv/solution.sql
@@ -1,13 +1,28 @@
 # Write your MySQL query statement below
 
-SELECT ROUND(count(*)/(SELECT COUNT(distinct player_id) FROM ACTIVITY), 2) as fraction
-FROM
-(SELECT player_id, count(*) as c
-FROM Activity
-WHERE (player_id, event_date) IN (SELECT player_id, date_add(min(event_date), interval 1 DAY) FROM Activity GROUP BY player_id)
-GROUP BY player_id) c1
+# SELECT 
+# ROUND(COUNT(DISTINCT player_id)/(SELECT count(*) FROM Activity WHERE (player_id, event_date) IN (SELECT player_id, date_add(min(event_date), interval 1 DAY) FROM Activity GROUP BY player_id)), 2)
+# FROM Activity
 
 # SELECT player_id, count(event_date) as c
 # FROM Activity
 # WHERE (player_id, event_date) IN (SELECT player_id, min(event_date) FROM Activity GROUP BY player_id)
 # GROUP BY player_id
+
+SELECT
+  ROUND(
+    COUNT(A1.player_id)
+    / (SELECT COUNT(DISTINCT A3.player_id) FROM Activity A3)
+  , 2) AS fraction
+FROM
+  Activity A1
+WHERE
+  (A1.player_id, A1.event_date) IN (
+    SELECT
+      A2.player_id,
+      DATE_ADD(MIN(A2.event_date), INTERVAL 1 DAY)
+    FROM
+      Activity A2
+    GROUP BY
+      A2.player_id
+  );
