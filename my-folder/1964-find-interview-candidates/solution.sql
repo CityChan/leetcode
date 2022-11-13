ecaaedd3c6237a03329b566543e1c4fc7b9b0b1d
CityChan <53466807+CityChan@users.noreply.github.com>
2022-11-13 01:31:55 +0000

    [LeetCode Sync] Runtime - 1866 ms (26.74%), Memory - 0B (100.00%)

--- my-folder/1964-find-interview-candidates/solution.sql
+++ my-folder/1964-find-interview-candidates/solution.sql
@@ -10,7 +10,7 @@ SELECT mail, name FROM Users
 WHERE user_id IN 
 (SELECT * FROM
 (
-    SELECT DISTINCT user_id FROM
+    SELECT user_id FROM
     (SELECT user_id, contest_id - row_number() over(PARTITION BY user_id ORDER BY contest_id) diff  
     FROM t1) t2
     GROUP BY user_id, diff
@@ -19,3 +19,30 @@ WHERE user_id IN
     SELECT gold_medal FROM Contests GROUP BY gold_medal HAVING COUNT(*)>=3
 ) t3)
 
+
+# WITH t1 AS
+# (
+#     SELECT contest_id, gold_medal AS user_id FROM Contests
+#     UNION
+#     SELECT contest_id, silver_medal FROM Contests
+#     UNION
+#     SELECT contest_id, bronze_medal FROM Contests
+# )
+
+# SELECT name, mail
+# FROM Users
+# WHERE user_id 
+# IN
+# (
+#     SELECT user_id
+#     FROM
+#     (
+#         SELECT user_id, contest_id - ROW_NUMBER() OVER (PARTITION BY user_id ORDER BY contest_id) AS diff
+#         FROM t1
+#     ) t2
+#     GROUP BY user_id, diff
+#     HAVING COUNT(*)>=3
+#     UNION ALL
+#     SELECT gold_medal FROM Contests GROUP BY gold_medal HAVING COUNT(*)>=3
+# )
+    
