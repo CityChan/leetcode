4603ddedc7f8a018661f4949ee2144790359ff5c
CityChan <53466807+CityChan@users.noreply.github.com>
2023-10-29 02:46:28 +0000

    [LeetCode Sync] Runtime - 2 ms (97.67%), Memory - 40.7 MB (99.98%)

--- my-folder/0020-valid-parentheses/solution.java
+++ my-folder/0020-valid-parentheses/solution.java
@@ -1,24 +1,35 @@
 class Solution {
+  
+    private Map<Character, Character> mappings; 
+  
+    public Solution() {
+      this.mappings = new HashMap<>();
+      this.mappings.put(')', '(');
+      this.mappings.put('}', '{');
+      this.mappings.put(']', '[');
+    }
+  
     public boolean isValid(String s) {
-        char[] stack = new char[s.length()];
-        int head = 0;
-        for (char c : s.toCharArray()) {
-            switch(c) {
-                case '(':
-                case '[':
-                case '{':
-                    stack[head++] = c;
-                    break;
-                case ')':
-                    if (head == 0 || stack[--head] != '(') return false;
-                    break;
-                case ']':
-                    if (head == 0 || stack[--head] != '[') return false;
-                    break;
-                case '}':
-                    if (head == 0 || stack[--head] != '{') return false;
-            }
+      if (s == null || s.length() == 0) {
+        return true;
+      }
+      
+      Stack<Character> stack = new Stack<>();
+      for (char c : s.toCharArray()) {
+        if (this.mappings.containsKey(c)) {
+          // look up top of stack 
+          if (stack.isEmpty()) {
+            return false;
+          }
+          // if not matching 
+          if (stack.peek() != this.mappings.get(c)) {
+            return false;
+          }
+          stack.pop();
+        } else {
+          stack.push(c);
         }
-        return head == 0;
+      }
+      return stack.isEmpty();
     }
 }
