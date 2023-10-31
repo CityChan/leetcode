b273badc75e9f92ed714fb2df1b1374e67b37820
CityChan <53466807+CityChan@users.noreply.github.com>
2023-10-31 04:24:54 +0000

    [LeetCode Sync] Runtime - 14 ms (99.91%), Memory - 38.8 MB (99.99%)

--- my-folder/0278-first-bad-version/solution.java
+++ my-folder/0278-first-bad-version/solution.java
@@ -3,16 +3,21 @@
 
 public class Solution extends VersionControl {
     public int firstBadVersion(int n) {
-        int start = 1, end = n;
-        while (start + 1 < end) {
-            int mid = start + (end - start) / 2;
-            if (isBadVersion(mid)) {
-                end = mid;
-            } else {
-                start = mid;
-            }
+      int start = 1;
+      int end = n;
+      
+      // exit case: start >= end
+      while (start < end) {
+        int mid = start + (end - start) / 2;
+        if (isBadVersion(mid)) {
+          // mid could be the right answer
+          end = mid;
+        } else {
+          // mid could not be the right anwer
+          start = mid + 1;
         }
-        if (isBadVersion(start)) return start;
-        return end;
+      }
+      
+      return isBadVersion(end) ? end : start;
     }
 }
