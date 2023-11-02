c3384f3736c107729d3e1b9b980d75ce08f854e1
CityChan <53466807+CityChan@users.noreply.github.com>
2023-11-02 04:03:07 +0000

    [LeetCode Sync] Runtime - 1 ms (99.41%), Memory - 58 MB (5.05%)

--- my-folder/0053-maximum-subarray/solution.java
+++ my-folder/0053-maximum-subarray/solution.java
@@ -1,15 +1,16 @@
 class Solution {
     public int maxSubArray(int[] nums) {
-        int currMaxSum = nums[0];
-        int max = nums[0];
-        for(int i=1;i<nums.length;i++){
-            if(currMaxSum>0){
-                currMaxSum += nums[i];
-            }else{
-                currMaxSum = nums[i];
-            }
-            max = Math.max(max, currMaxSum);
+      int currMaxSum = nums[0];
+      int max = nums[0];
+      
+      for (int i = 1; i < nums.length; i++) {
+        if (currMaxSum > 0) {
+          currMaxSum += nums[i];
+        } else {
+          currMaxSum = nums[i];
         }
-        return max;
+        max = Math.max(max, currMaxSum);
+      }
+      return max;
     }
 }
