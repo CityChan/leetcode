67f1b1cf0e51552edebfc360c18c481e66f33c17
CityChan <53466807+CityChan@users.noreply.github.com>
2024-01-06 23:29:19 +0000

    [LeetCode Sync] Runtime - 525 ms (5.01%), Memory - 30.1 MB (10.79%)

--- my-folder/0011-container-with-most-water/solution.py
+++ my-folder/0011-container-with-most-water/solution.py
@@ -1,20 +1,12 @@
-class Solution(object):
-    def maxArea(self, height):
-        """
-        :type height: List[int]
-        :rtype: int
-        """
-        left_idx, right_idx = 0, len(height)-1
-        water = 0
-        while left_idx<right_idx:
-            water = max(water, min(height[left_idx], height[right_idx])*(right_idx-left_idx))
-            if height[left_idx]<height[right_idx]:
-                left_idx+=1
-            elif height[left_idx]>height[right_idx]:
-                right_idx-=1
+class Solution:
+    def maxArea(self, height: List[int]) -> int:
+        left = 0
+        right = len(height)-1
+        max_water = 0
+        while left<right:
+            max_water = max(max_water, min(height[left], height[right])*(right-left))
+            if height[left]<height[right]:
+                left+=1
             else:
-                left_idx+=1
-                right_idx-=1
-        return water
-                
-        
+                right-=1
+        return max_water
