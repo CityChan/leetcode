a75b7f75a3299df2f183f7a906dfb4edde8693e8
CityChan <53466807+CityChan@users.noreply.github.com>
2024-01-07 21:26:48 +0000

    [LeetCode Sync] Runtime - 38 ms (1.84%), Memory - 17.4 MB (57.94%)

--- my-folder/1528-kids-with-the-greatest-number-of-candies/solution.py
+++ my-folder/1528-kids-with-the-greatest-number-of-candies/solution.py
@@ -1,14 +1,8 @@
 class Solution:
     def kidsWithCandies(self, candies: List[int], extraCandies: int) -> List[bool]:
-        result = [True] * len(candies)
-        max = candies[0]
-        for i in range(1, len(candies)):
-            if candies[i] > max:
-                max = candies[i]
-                
-        for j in range(len(candies)):
-            if candies[j] == max or candies[j] + extraCandies >= max:
-                result[j] = True
-            else:
-                result[j] = False
-        return result
+        i_max = 0
+        for i in range(len(candies)):
+            if candies[i]>=candies[i_max]:
+                i_max = i
+        return [x+extraCandies>=candies[i_max] for x in candies]
+        
