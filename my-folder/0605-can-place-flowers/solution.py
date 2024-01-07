f9e8f185e6f0bde421827887f98432ef28ddad68
CityChan <53466807+CityChan@users.noreply.github.com>
2024-01-07 22:07:10 +0000

    [LeetCode Sync] Runtime - 132 ms (6.50%), Memory - 17.6 MB (62.90%)

--- my-folder/0605-can-place-flowers/solution.py
+++ my-folder/0605-can-place-flowers/solution.py
@@ -1,9 +1,10 @@
 class Solution:
     def canPlaceFlowers(self, flowerbed: List[int], n: int) -> bool:
-        pad_flowerbed = [0] + flowerbed + [0]
-        count = 0
         for i in range(len(flowerbed)):
-            if pad_flowerbed[i:i+3] == [0, 0, 0]:
-                pad_flowerbed[i+1] = 1
-                count += 1
-        return count >= n
+            if (flowerbed[i]==1) or (i-1>=0 and flowerbed[i-1]==1) or (i+1<=len(flowerbed)-1 and flowerbed[i+1]==1):
+                continue
+            else:
+                flowerbed[i]=1
+                n-=1
+        return n<=0
+        
