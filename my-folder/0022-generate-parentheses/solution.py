e52908799a1607fdbd0aa55f49ed78fc1c1b1a9f
CityChan <53466807+CityChan@users.noreply.github.com>
2024-04-12 20:03:00 +0000

    [LeetCode Sync] Runtime - 42 ms (5.31%), Memory - 16.7 MB (97.79%)

--- my-folder/0022-generate-parentheses/solution.py
+++ my-folder/0022-generate-parentheses/solution.py
@@ -1,23 +1,13 @@
-class Solution(object):
-    def generateParenthesis(self, n):
-        """
-        :type n: int
-        :rtype: List[str]
-        """
-    #         def generate(p, left, right, parens=[]):
-    #     if left:         generate(p + '(', left-1, right)
-    #     if right > left: generate(p + ')', left, right-1)
-    #     if not right:    parens += p,
-    #     return parens
-    # return generate('', n, n)
-        
+class Solution:
+    def generateParenthesis(self, n: int) -> List[str]:
         res = []
-        def traverse(now, left, right):
-            if left+right==2*n:
-                res.append(now)
-            if left<n:
-                traverse(now+"(", left+1, right)
-            if right<left:
-                traverse(now+")", left, right+1)
+        def traverse(now_str, l, r):
+            if len(now_str)==2*n:
+                res.append(now_str)
+            if l<n:
+                traverse(now_str+"(", l+1, r)
+            if r<l:
+                traverse(now_str+")", l, r+1)
         traverse("", 0, 0)
         return res
+        
