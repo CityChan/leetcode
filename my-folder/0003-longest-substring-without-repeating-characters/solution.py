426654f897469fd076c3d58d3290cd46af4b4e1f
CityChan <53466807+CityChan@users.noreply.github.com>
2024-05-18 23:58:47 +0000

    [LeetCode Sync] Runtime - 79 ms (13.12%), Memory - 16.8 MB (86.37%)

--- my-folder/0003-longest-substring-without-repeating-characters/solution.py
+++ my-folder/0003-longest-substring-without-repeating-characters/solution.py
@@ -1,23 +1,21 @@
-class Solution(object):
-    def lengthOfLongestSubstring(self, s):
-        """
-        :type s: str
-        :rtype: int
-        """
-        if len(s)<2:
-            return len(s)
-        save_dict = {s[0]: 0}
-        start, end = 0, 1
-        max_len = 1
-        while end<len(s):
-            if (s[end] in save_dict) and (save_dict[s[end]]>=start):
-                start = save_dict[s[end]] + 1
-                           
-            save_dict[s[end]] = end
-            max_len = max(max_len, end-start+1)
-            
-            end+=1
-            
-        return max_len
-            
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        max_len = 0
+        left = 0
+        right = 0
+        char_set = set()
+        while right<len(s):
+            if s[right] not in char_set:
+                char_set.add(s[right])
+                if right==len(s)-1:
+                    max_len = max(max_len, right-left+1)
+            else:
+                max_len = max(max_len, right-left)
+                while s[left]!=s[right]:
+                    print(char_set)
+                    char_set.remove(s[left])
+                    left+=1
+                left+=1
+            right+=1
+        return max(max_len, right-left)
         
