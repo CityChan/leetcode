2b083199ec98ed69621fe443069feb0304b46fbe
CityChan <53466807+CityChan@users.noreply.github.com>
2024-05-18 13:57:51 +0000

    [LeetCode Sync] Runtime - 246 ms (40.47%), Memory - 27 MB (94.21%)

--- my-folder/0200-number-of-islands/solution.py
+++ my-folder/0200-number-of-islands/solution.py
@@ -4,66 +4,19 @@ class Solution(object):
         :type grid: List[List[str]]
         :rtype: int
         """
-        # count = 0
-        # def search_p(grid, i, j):
-        #     if i>=0 and i<len(grid) and j>=0 and j<len(grid[0]) and grid[i][j] == '1':
-        #             grid[i][j] = '0'
-        #             for ii, jj in ((0, 1), (0, -1), (1, 0), (-1, 0)):
-        #                 search_p(grid, i+ii, j+jj)
-        # for i in range(len(grid)):
-        #     for j in range(len(grid[0])):
-        #         if grid[i][j] == '1':
-        #             count +=1
-        #             search_p(grid, i, j)
-        # return count
+
         count = 0
         for i in range(len(grid)):
             for j in range(len(grid[0])):
-                if grid[i][j]=='1':
-                    count+=1
-                    grid[i][j]='0'
+                if grid[i][j]=="1":
                     q = [(i, j)]
                     while q:
-
                         ii, jj = q.pop(0)
-                        for di, dj in ((-1, 0), (1, 0), (0, 1), (0, -1)):
-       
-                            nxt_i = ii+di
-                            nxt_j = jj+dj
-         
-                            if 0<=nxt_i<len(grid) and 0<=nxt_j<len(grid[0]) and grid[nxt_i][nxt_j]=='1':
-        
-                                grid[nxt_i][nxt_j]='0'
-                                q.append((nxt_i, nxt_j))
-      
+                        if 0<=ii<len(grid) and 0<=jj<len(grid[0]):
+                            if grid[ii][jj]=="1":
+                                grid[ii][jj]="0"
+                                q.extend([(ii-1, jj), (ii+1, jj), (ii, jj-1), (ii, jj+1)])
+                    count+=1
         return count
-                
-            
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
+                    
         
