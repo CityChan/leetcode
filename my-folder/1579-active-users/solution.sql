d4ea3fa6f6329e02e135adabe95d93112ca4c593
CityChan <53466807+CityChan@users.noreply.github.com>
2024-05-19 14:56:59 +0000

    [LeetCode Sync] Runtime - 3024 ms (17.70%), Memory - 0B (100.00%)

--- my-folder/1579-active-users/solution.sql
+++ my-folder/1579-active-users/solution.sql
@@ -1,9 +1,19 @@
 # Write your MySQL query statement below
-SELECT l3.id, name FROM
-(SELECT DISTINCT id FROM
-(SELECT id, date_sub(login_date, interval row_number() OVER (PARTITION BY id) DAY) group_date FROM
-(SELECT DISTINCT id, login_date FROM Logins ORDER BY id, login_date) l1) l2
-GROUP BY id, group_date
-HAVING count(*)>=5) l3
-LEFT JOIN Accounts a
-ON l3.id = a.id
+# SELECT l3.id, name FROM
+# (SELECT DISTINCT id FROM
+# (SELECT id, date_sub(login_date, interval row_number() OVER (PARTITION BY id) DAY) group_date FROM
+# (SELECT DISTINCT id, login_date FROM Logins ORDER BY id, login_date) l1) l2
+# GROUP BY id, group_date
+# HAVING count(*)>=5) l3
+# LEFT JOIN Accounts a
+# ON l3.id = a.id
+
+SELECT DISTINCT a.id, (SELECT name FROM Accounts WHERE id=a.id) name
+FROM Logins a, Logins b
+WHERE a.id=b.id
+AND DATEDIFF(a.login_date, b.login_date) BETWEEN 1 AND 4
+GROUP BY a.id, a.login_date
+HAVING COUNT(DISTINCT b.login_date) = 4
+
+
+
