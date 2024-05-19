b4162f08c8c4095b540258378147a7cc5b1b41a1
CityChan <53466807+CityChan@users.noreply.github.com>
2024-05-19 20:12:15 +0000

    [LeetCode Sync] Runtime - 591 ms (14.87%), Memory - 0B (100.00%)

--- my-folder/0176-second-highest-salary/solution.sql
+++ my-folder/0176-second-highest-salary/solution.sql
@@ -1,7 +1,36 @@
 # Write your MySQL query statement below
-SELECT max(salary) AS SecondHighestSalary
-From Employee
-Where salary < (SELECT max(salary) FROM Employee)
+# SELECT max(salary) AS SecondHighestSalary
+# From Employee
+# Where salary < (SELECT max(salary) FROM Employee)
 # SELECT DISTINCT e1.salary AS SecondHighestSalary
 # FROM Employee e1
 # WHERE 1= (SELECT COUNT(DISTINCT e2.salary) FROM Employee e2 WHERE e1.salary<e2.salary)
+# SELECT salary SecondHighestSalary FROM
+# (SELECT salary, dense_rank() OVER (ORDER BY salary DESC) 'ranking'
+# FROM Employee e1) e2
+# WHERE e2.ranking=2
+
+
+# SELECT FIRST_VALUE(salary) OVER(ORDER BY salary) AS SecondHighestSalary
+# FROM Employee e1 
+# WHERE 1 = (SELECT COUNT(DISTINCT e2.salary) FROM Employee e2 WHERE e2.salary>e1.salary)
+
+# SELECT IFNULL((SELECT DISTINCT SALARY FROM EMPLOYEE ORDER BY SALARY DESC LIMIT 1, 1), NULL)
+# FROM Employee e1
+
+# SELECT IFNULL(
+#     (SELECT DISTINCT salary SecondHighestSalary
+#      FROM Employee e1
+#      WHERE (SELECT COUNT(DISTINCT e2.salary) 
+#             FROM Employee e2 
+#             WHERE e1.salary < e2.salary) = 1), NULL) SecondHighestSalary;
+
+SELECT IFNULL(
+(SELECT DISTINCT salary
+FROM
+(SELECT salary, DENSE_RANK() OVER (ORDER BY salary DESC) ranking
+FROM Employee) e
+WHERE e.ranking=2), NULL) SecondHighestSalary 
+
+
+
