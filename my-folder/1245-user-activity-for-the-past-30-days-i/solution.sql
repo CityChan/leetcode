0c6977c6b807332cb0c04d94c7bf5a903c6649e8
CityChan <53466807+CityChan@users.noreply.github.com>
2024-05-19 13:58:00 +0000

    [LeetCode Sync] Runtime - 1021 ms (16.80%), Memory - 0B (100.00%)

--- my-folder/1245-user-activity-for-the-past-30-days-i/solution.sql
+++ my-folder/1245-user-activity-for-the-past-30-days-i/solution.sql
@@ -1,5 +1,12 @@
 # Write your MySQL query statement below
+# SELECT activity_date day, COUNT(DISTINCT user_id) active_users
+# FROM Activity
+# WHERE DATEDIFF('2019-07-27', activity_date)<30 AND DATEDIFF('2019-07-27', activity_date)>=0
+# GROUP BY activity_date
+
+
+
 SELECT activity_date day, COUNT(DISTINCT user_id) active_users
 FROM Activity
-WHERE DATEDIFF('2019-07-27', activity_date)<30 AND DATEDIFF('2019-07-27', activity_date)>=0
+WHERE activity_date BETWEEN DATE_SUB("2019-07-27", INTERVAL 29 DAY) AND "2019-07-27"
 GROUP BY activity_date
