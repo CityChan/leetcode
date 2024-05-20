cf87387abb87a285d33e0f3e9ab4085a1d989068
CityChan <53466807+CityChan@users.noreply.github.com>
2024-05-20 03:05:51 +0000

    [LeetCode Sync] Runtime - 634 ms (24.84%), Memory - 0B (100.00%)

--- my-folder/0626-exchange-seats/solution.sql
+++ my-folder/0626-exchange-seats/solution.sql
@@ -1,9 +1,9 @@
 # Write your MySQL query statement below
-SELECT id, 
-CASE WHEN mod(id, 2)=1 and id<(SELECT max(id) FROM Seat) THEN LEAD(student, 1) OVER()
-WHEN mod(id, 2)=1 and id=(SELECT max(id) FROM Seat) THEN student
-ELSE LAG(student, 1) OVER() END student
-FROM Seat
+# SELECT id, 
+# CASE WHEN mod(id, 2)=1 and id<(SELECT max(id) FROM Seat) THEN LEAD(student, 1) OVER()
+# WHEN mod(id, 2)=1 and id=(SELECT max(id) FROM Seat) THEN student
+# ELSE LAG(student, 1) OVER() END student
+# FROM Seat
 
 # Select case
 # when id%2=0 then id-1
@@ -13,3 +13,17 @@ FROM Seat
 # , student
 # from seat
 # order by id asc
+
+# SELECT
+# CASE WHEN MOD(id, 2)=0 THEN id-1 
+# WHEN MOD(id, 2)=1 AND id!=(SELECT MAX(id) FROM Seat) THEN id+1
+# ELSE id END id, student
+# FROM Seat
+# ORDER BY ID
+
+SELECT id,
+CASE WHEN MOD(id, 2)=1 AND id!=max_id THEN LEAD(student, 1) OVER()
+WHEN MOD(id, 2)=0 THEN LAG(student, 1) OVER()
+ELSE student END student
+FROM Seat, (SELECT MAX(id) max_id FROM Seat) MaxID
+
