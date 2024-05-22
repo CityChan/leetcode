670bebb58ca7faa8926e2edf74f3dfc946349bb5
CityChan <53466807+CityChan@users.noreply.github.com>
2024-05-22 20:46:09 +0000

    [LeetCode Sync] Runtime - 2141 ms (16.01%), Memory - 0B (100.00%)

--- my-folder/0185-department-top-three-salaries/solution.sql
+++ my-folder/0185-department-top-three-salaries/solution.sql
@@ -1,5 +1,20 @@
 # Write your MySQL query statement below
-SELECT d.name Department, e1.name Employee, e1.salary Salary
+# SELECT d.name Department, e1.name Employee, e1.salary Salary
+# FROM Employee e1, Department d
+# WHERE 3>(SELECT count(DISTINCT e2.salary) FROM Employee e2 WHERE e1.salary<e2.salary and e1.departmentID=e2.departmentID)
+# and e1.departmentId = d.id
+
+
+# SELECT d.name AS Department, e.name AS Employee, salary AS Salary
+# FROM
+# (
+#     SELECT *, DENSE_RANK() OVER (PARTITION BY departmentId ORDER BY salary DESC) AS ranking
+#     FROM Employee 
+# ) e, Department d
+# WHERE e.ranking < 4 and e.departmentId = d.id
+
+
+SELECT d.name AS Department, e1.name AS Employee, salary AS Salary
 FROM Employee e1, Department d
-WHERE 3>(SELECT count(DISTINCT e2.salary) FROM Employee e2 WHERE e1.salary<e2.salary and e1.departmentID=e2.departmentID)
-and e1.departmentId = d.id
+WHERE 3>(SELECT COUNT(DISTINCT e2.salary) FROM Employee e2 WHERE e1.salary<e2.salary AND e1.departmentId=e2.departmentId)
+AND e1.departmentId = d.id
