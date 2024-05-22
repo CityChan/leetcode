fb54e7e710f9b52b2bfafddc45c73d84cd282411
CityChan <53466807+CityChan@users.noreply.github.com>
2024-05-22 20:25:46 +0000

    [LeetCode Sync] Runtime - 645 ms (45.45%), Memory - 0B (100.00%)

--- my-folder/0197-rising-temperature/solution.sql
+++ my-folder/0197-rising-temperature/solution.sql
@@ -1,5 +1,16 @@
 # Write your MySQL query statement below
-SELECT w1.ID 
+# SELECT w1.ID 
+# FROM Weather w1, Weather w2
+# WHERE DATEDIFF(w1.recordDate, w2.recordDate)=1
+# and w1.temperature>w2.temperature
+
+# SELECT id FROM
+# (SELECT *, LAG(temperature, 1) OVER(ORDER BY recordDate) AS lag_t
+# FROM Weather) w
+# WHERE temperature>lag_t
+
+
+SELECT w1.id
 FROM Weather w1, Weather w2
-WHERE DATEDIFF(w1.recordDate, w2.recordDate)=1
-and w1.temperature>w2.temperature
+WHERE w1.recordDate = DATE_ADD(w2.recordDate, INTERVAL 1 DAY)
+AND w1.temperature>w2.temperature
