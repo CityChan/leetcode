b39eee9d6d5960557dd9fd030e5b2ed717686bc5
CityChan <53466807+CityChan@users.noreply.github.com>
2024-05-23 22:24:48 +0000

    [LeetCode Sync] Runtime - 1371 ms (22.12%), Memory - 0B (100.00%)

--- my-folder/0184-department-highest-salary/solution.sql
+++ my-folder/0184-department-highest-salary/solution.sql
@@ -1,9 +1,9 @@
 # Write your MySQL query statement below
-SELECT d.name department, e2.name employee, e2.SALARY
-FROM 
-EMPLOYEE E2, 
-(SELECT MAX(SALARY) as SALARY, departmentid FROM EMPLOYEE GROUP BY DEPARTMENTID) E1,
-department d
-where E2.SALARY= E1.salary and e2.departmentid=E1.departmentid and e2.departmentid=d.id
-
-
+SELECT d.name AS Department, e2.name AS Employee, e2.salary AS Salary
+FROM
+(SELECT departmentID, MAX(salary) AS max_salary
+FROM Employee
+GROUP BY departmentId) e1,
+Employee e2,
+Department d
+WHERE e2.departmentId = e1.departmentId AND e2.salary = e1.max_salary AND e2.departmentId = d.id
