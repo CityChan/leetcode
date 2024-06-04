6a9d0326e9429c86fc0fcec40af7edd3a8c195c1
CityChan <53466807+CityChan@users.noreply.github.com>
2024-06-04 00:24:21 +0000

    [LeetCode Sync] Runtime - 36 ms (2.44%), Memory - 16.3 MB (99.96%)

--- my-folder/0198-house-robber/solution.py
+++ my-folder/0198-house-robber/solution.py
@@ -1,9 +1,16 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        prev_max = 0
-        prev_max_skip = 0
-        for i in nums:
-            # final_max = max(prev_max_skip+nums[i], prev_max)
-            prev_max, prev_max_skip = max(prev_max_skip+i, prev_max), prev_max
-        return max(prev_max_skip, prev_max)
+        # prev_max = 0
+        # prev_max_skip = 0
+        # for i in nums:
+        #     # final_max = max(prev_max_skip+nums[i], prev_max)
+        #     prev_max, prev_max_skip = max(prev_max_skip+i, prev_max), prev_max
+        # return max(prev_max_skip, prev_max)
+        
+        i1 = 0
+        i2 = 0
+        for i in range(len(nums)):
+            i1, i2 = i2, max(i1+nums[i], i2) 
+                
+        return max(i1, i2)
         
