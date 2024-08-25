f087258b7cc8af99609e41585e98beea6b300772
CityChan <53466807+CityChan@users.noreply.github.com>
2024-08-25 16:42:21 +0000

    [LeetCode Sync] Runtime - 145 ms (5.37%), Memory - 17.1 MB (98.28%)

--- my-folder/0694-number-of-distinct-islands/solution.py
+++ my-folder/0694-number-of-distinct-islands/solution.py
@@ -1,35 +1,28 @@
-class Solution(object):
-    def numDistinctIslands(self, grid):
-        """
-        :type grid: List[List[int]]
-        :rtype: int
-        """
-        def dfs(grid, i, j, d):
-            if 0<=i<len(grid) and 0<=j<len(grid[0]):
-                if grid[i][j]==1:
-                    
-                    path=d
-                    # print(path)
-                    grid[i][j]=0
-                    path+=dfs(grid, i-1, j, 'u')
-                    # print(path)
-                    path+=dfs(grid, i+1, j, 'd')
-                    # print(path)
-                    path+=dfs(grid, i, j-1, 'l')
-                    # print(path)
-                    path+=dfs(grid, i, j+1, 'r')
-                    # print(path)
-                    path+="b"
-                    
-                    return path
-            return ""
-        total_path = set()
-        for i in range(len(grid)):
-            for j in range(len(grid[0])):
-                if grid[i][j]==1:
-                    # path = ""
-                    
-                    total_path.add(dfs(grid, i, j, 'o'))
-        # print(set([dfs(grid, i, j, 'o') for i in range(len(grid)) for j in range(len(grid[0]))]))
-        return len(total_path)
+class Solution:
+    def numDistinctIslands(self, grid: List[List[int]]) -> int:
+        m = len(grid)
+        n = len(grid[0])
+        islands = set()
+        for i in range(m):
+            for j in range(n):
+                if grid[i][j] == 1:
+                    sb = []
+                    self.dfs(grid,i,j,sb,0)
+                    islands.add(tuple(sb))
+        return len(islands)
+        
+    def dfs(self, grid, i,j, sb,d):
+        m = len(grid)
+        n = len(grid[0])
+        if i < 0 or i >= m or j < 0 or j >=n:
+            return 
+        if grid[i][j] == 0:
+            return
+        grid[i][j] = 0
+        sb.append(d)
+        self.dfs(grid, i+1, j, sb, 1)
+        self.dfs(grid, i-1, j, sb, 2)
+        self.dfs(grid, i, j+1, sb, 3)
+        self.dfs(grid, i, j-1, sb, 4)
+        sb.append(-d)
         
