ac99410a75ec97f30c57e8baedbecb453e608f34
CityChan <53466807+CityChan@users.noreply.github.com>
2024-08-25 21:11:08 +0000

    [LeetCode Sync] Runtime - 104 ms (5.49%), Memory - 16.9 MB (97.66%)

--- my-folder/0695-max-area-of-island/solution.py
+++ my-folder/0695-max-area-of-island/solution.py
@@ -1,32 +1,20 @@
-class Solution(object):
-    def maxAreaOfIsland(self, grid):
-        """
-        :type grid: List[List[int]]
-        :rtype: int
-        """
-        # count = 0
-        max_area = 0
-        for i in range(len(grid)):
-            for j in range(len(grid[0])):
-                if grid[i][j]==1:
-                    area = 0
-                    # count+=1
-                    grid[i][j]=0
-                    q = [(i, j)]
-                    while q:
-                        ii, jj = q.pop(0)
-                        area+=1
-                        for di, dj in ((-1, 0), (1, 0), (0, 1), (0, -1)):
-       
-                            nxt_i = ii+di
-                            nxt_j = jj+dj
-         
-                            if 0<=nxt_i<len(grid) and 0<=nxt_j<len(grid[0]) and grid[nxt_i][nxt_j]==1:
-                                # area+=1
-                                grid[nxt_i][nxt_j]=0
-                                q.append((nxt_i, nxt_j))
-                    max_area = max(max_area, area)
-        
-        
-        return max_area
-        
+class Solution:
+    def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
+        m = len(grid)
+        n = len(grid[0])
+        res = 0
+        for i in range(m):
+            for j in range(n):
+                if grid[i][j] == 1:
+                    res = max(res,self.dfs(grid, i,j))
+        return res
+    
+    def dfs(self, grid, i, j):
+        m = len(grid)
+        n = len(grid[0])
+        if i < 0 or i >= m or j < 0 or j >= n:
+            return 0
+        if grid[i][j] == 0:
+            return 0
+        grid[i][j] = 0
+        return self.dfs(grid, i+1,j) + self.dfs(grid, i-1,j) + self.dfs(grid, i,j+1) + self.dfs(grid, i,j-1) + 1
