5a2d914cd360a05e315a18557f4811848bc988b8
CityChan <53466807+CityChan@users.noreply.github.com>
2024-10-12 02:37:50 +0000

    [LeetCode Sync] Runtime - 38 ms (0.08%), Memory - 16.5 MB (96.76%)

--- my-folder/0054-spiral-matrix/solution.py
+++ my-folder/0054-spiral-matrix/solution.py
@@ -1,28 +1,23 @@
 class Solution:
     def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
-        u = 0
-        d = len(matrix)-1
-        l = 0
-        r = len(matrix[0])-1
-        res = []
-        while len(res)<len(matrix)*len(matrix[0]):
-            if u<=d:
-                for i in range(l, r+1):
-                    res.append(matrix[u][i])
-                u+=1
-            if l<=r:
-                for i in range(u, d+1):
-                    res.append(matrix[i][r])
-                r-=1
-            if u<=d:
-                for i in reversed(range(l, r+1)):
-                    res.append(matrix[d][i])
-                d-=1
-            if l<=r:
-                for i in reversed(range(u, d+1)):
-                    res.append(matrix[i][l])
-                l+=1
-            # print(u, d, l, r)
-        return res
-                
+        m,n = len(matrix), len(matrix[0])
+        visited = []
+        visited_node = 0
+        cur = [0,0]
+        dirs = [[0,1],[1,0],[0,-1],[-1,0]]
+        dir_num = 0
+        ans = []
+        while visited_node < m*n:
+            visited_node += 1
+            i,j = cur[0],cur[1]
+            visited.append((i,j))
+            ans.append(matrix[i][j])
+            cur[0], cur[1] = i + dirs[dir_num][0], j + dirs[dir_num][1]
+            if cur[0] < 0 or cur[0] >= m or cur[1] < 0 or cur[1] >= n or (cur[0],cur[1]) in visited:
+                dir_num = (dir_num + 1) % 4
+                cur[0], cur[1] = i + dirs[dir_num][0], j + dirs[dir_num][1]
+        return ans
             
+        
+            
+        
