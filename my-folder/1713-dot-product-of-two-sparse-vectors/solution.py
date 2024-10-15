145026e8b162648deba1551e47470538f4d712bb
CityChan <53466807+CityChan@users.noreply.github.com>
2024-10-15 05:31:24 +0000

    [LeetCode Sync] Runtime - 1287 ms (85.18%), Memory - 20.8 MB (87.59%)

--- my-folder/1713-dot-product-of-two-sparse-vectors/solution.py
+++ my-folder/1713-dot-product-of-two-sparse-vectors/solution.py
@@ -1,17 +1,17 @@
 class SparseVector:
     def __init__(self, nums: List[int]):
-        self.nums_dict = {i:n for i, n in enumerate(nums) if n!=0}
-        
+        self.nums_dict = {}
+        for i,num in enumerate(nums):
+            if num != 0:
+                self.nums_dict[i] = num
 
     # Return the dotProduct of two sparse vectors
     def dotProduct(self, vec: 'SparseVector') -> int:
-        s = 0
-        for i in self.nums_dict:
-            if i in vec.nums_dict:
-                s+=self.nums_dict[i]*vec.nums_dict[i]
-        return s
-        
-
+        dotproduct = 0
+        for key in vec.nums_dict:
+            if key in self.nums_dict:
+                dotproduct += vec.nums_dict[key]*self.nums_dict[key]
+        return dotproduct
 # Your SparseVector object will be instantiated and called as such:
 # v1 = SparseVector(nums1)
 # v2 = SparseVector(nums2)
