51c152ae4ecd400b63eadbd43c90c0a1aa867063
CityChan <53466807+CityChan@users.noreply.github.com>
2024-10-18 20:53:10 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 16.6 MB (91.20%)

--- my-folder/0017-letter-combinations-of-a-phone-number/solution.py
+++ my-folder/0017-letter-combinations-of-a-phone-number/solution.py
@@ -1,30 +1,25 @@
-class Solution(object):
-    def letterCombinations(self, digits):
-        """
-        :type digits: str
-        :rtype: List[str]
-        """
-        letter_dict = {"2": "abc", "3": "def", "4": "ghi", "5": "jkl", "6": "mno",
-                      "7": "pqrs", "8": "tuv", "9": "wxyz"}
-        # def recur(digits):
-        #     if len(digits)==1:
-        #         return list(letter_dict[digits])
-        #     elif len(digits)>=2:
-        #         res = []
-        #         left_res = recur(digits[:-1])
-        #         for i in left_res:
-        #             for j in letter_dict[digits[-1]]:
-        #                 res.append(i+j)
-        #         return res
-        # return recur(digits)
-        
-        if len(digits)==0:
-            return []
-        res = ['']
-        for num in digits:
-            res = [prev+now for now in letter_dict[num] for prev in res]
-        return res
-            
-                
+class Solution:
+    
+    def __init__(self):
+        self.res = []
+        self.sb = []
+        self.mapping = ["", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"]
 
+    def letterCombinations(self, digits: str) -> List[str]:
+        if not digits:
+            return self.res
+        self.backtrack(digits, 0)
+        return self.res
         
+    def backtrack(self, digits, start):
+        if len(self.sb) == len(digits):
+            self.res.append("".join(self.sb))
+            return
+        digit = int(digits[start])
+        letters = self.mapping[digit]
+        for letter in letters:
+            self.sb.append(letter)
+            self.backtrack(digits, start + 1)
+            self.sb.pop()
+        return
+                
