9a1be832104499425cee5c73167e337230c11a73
CityChan <53466807+CityChan@users.noreply.github.com>
2024-10-19 23:18:50 +0000

    [LeetCode Sync] Runtime - 239 ms (5.02%), Memory - 25.9 MB (91.54%)

--- my-folder/0121-best-time-to-buy-and-sell-stock/solution.py
+++ my-folder/0121-best-time-to-buy-and-sell-stock/solution.py
@@ -1,16 +1,13 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
         n = len(prices)
-        dp = [[0] * 2 for _ in range(n)]
-        
+        dp = [[0]* 2]*n
         for i in range(n):
-            if i - 1 == -1:
-                # base case
-                dp[i][0] = 0
-                dp[i][1] = -prices[i]
+            if i == 0:
+                dp[0][0] = 0
+                dp[0][1] = -prices[i]
                 continue
-                
-            dp[i][0] = max(dp[i - 1][0], dp[i - 1][1] + prices[i])
-            dp[i][1] = max(dp[i - 1][1], -prices[i])
-            
+            dp[i][0] = max(dp[i-1][0], dp[i-1][1] + prices[i])
+            dp[i][1] = max(-prices[i], dp[i-1][1])
         return dp[n-1][0]
+
