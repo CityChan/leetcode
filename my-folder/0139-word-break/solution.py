00675b18495056af742f69318f44d99385ccc6f0
CityChan <53466807+CityChan@users.noreply.github.com>
2024-10-23 23:59:15 +0000

    [LeetCode Sync] Runtime - 7 ms (23.44%), Memory - 16.7 MB (96.21%)

--- my-folder/0139-word-break/solution.py
+++ my-folder/0139-word-break/solution.py
@@ -1,17 +1,8 @@
 class Solution:
     def wordBreak(self, s: str, wordDict: List[str]) -> bool:
-        bool_res_list = [True]
-        for i in range(len(s)):
-            flag = 0
-            for w in wordDict:
-                if i-len(w)+1<0:
-                    continue
-                if s[i-len(w)+1:i+1]==w and bool_res_list[i-len(w)+1]:
-                    flag = 1
-                    break
-            if flag==1:
-                bool_res_list.append(True)
-            else:
-                bool_res_list.append(False)
-        return bool_res_list[-1]
-        
+        words = set(wordDict)
+        n = len(s)
+        dp = [True] + [False for _ in range(n)]
+        for i in range(1, n+1):
+            dp[i] = any(dp[j] and s[j:i] in words for j in range(i))
+        return dp[n]
