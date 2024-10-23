967100e06a47b6e83561aa6d429daf8a98f5bfca
CityChan <53466807+CityChan@users.noreply.github.com>
2024-10-23 23:11:58 +0000

    [LeetCode Sync] Runtime - 251 ms (50.88%), Memory - 19.1 MB (89.99%)

--- my-folder/0200-number-of-islands/solution.py
+++ my-folder/0200-number-of-islands/solution.py
@@ -1,22 +1,24 @@
-class Solution(object):
-    def numIslands(self, grid):
-        """
-        :type grid: List[List[str]]
-        :rtype: int
-        """
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
+        m,n = len(grid), len(grid[0])
+        visited = [[0 for _ in range(n)] for _ in range(m)]
+        dirs = [[1,0], [0,1], [-1, 0], [0, -1]]
+        def dps(grid, i, j):
+            visited[i][j] = 1
+            grid[i][j] = '0'
+            for d in dirs:
+                x, y = i + d[0], j + d[1]
+                if x < 0 or x >= m or y < 0 or y >= n:
+                    continue
+                if visited[x][y] == 0 and grid[x][y] == '1':
+                    dps(grid, x,y)
+        ans = 0
+        for k in range(m):
+            for l in range(n):
+                if k < 0 or k >= m or l < 0 or l >= n or grid[k][l] == '0':
+                    continue
+                ans +=1
+                dps(grid, k, l)
+        return ans
 
-        count = 0
-        for i in range(len(grid)):
-            for j in range(len(grid[0])):
-                if grid[i][j]=="1":
-                    q = [(i, j)]
-                    while q:
-                        ii, jj = q.pop(0)
-                        if 0<=ii<len(grid) and 0<=jj<len(grid[0]):
-                            if grid[ii][jj]=="1":
-                                grid[ii][jj]="0"
-                                q.extend([(ii-1, jj), (ii+1, jj), (ii, jj-1), (ii, jj+1)])
-                    count+=1
-        return count
-                    
-        
+
