35c90e94a3c03a6dbb591510163524fc91ab7091
CityChan <53466807+CityChan@users.noreply.github.com>
2024-10-23 01:35:09 +0000

    [LeetCode Sync] Runtime - 4 ms (14.45%), Memory - 16.6 MB (96.40%)

---
+++ my-folder/0249-group-shifted-strings/solution.py
@@ -0,0 +1,15 @@
+class Solution:
+    def groupStrings(self, strings: List[str]) -> List[List[str]]:
+        ans = []
+        hashmap = {}
+        for s in strings:
+            new_s = ""
+            first_char = s[0]
+            diff = ord(s[0]) - ord('a')
+            for c in s:
+                new_s += chr(ord(c) - diff if ord(c) - diff >= ord('a') else ord(c) - diff + 26)
+            if new_s in hashmap:
+                hashmap[new_s].append(s)
+            else:
+                hashmap[new_s] = [s]
+        return list(hashmap.values())
