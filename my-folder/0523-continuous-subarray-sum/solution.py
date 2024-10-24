8bcedbf7537a29b7a8d27baa518e82faec2b5dad
CityChan <53466807+CityChan@users.noreply.github.com>
2024-10-24 16:34:24 +0000

    [LeetCode Sync] Runtime - 63 ms (19.93%), Memory - 37.1 MB (92.47%)

--- my-folder/0523-continuous-subarray-sum/solution.py
+++ my-folder/0523-continuous-subarray-sum/solution.py
@@ -2,12 +2,13 @@ class Solution:
     def checkSubarraySum(self, nums: List[int], k: int) -> bool:
         hmap = {0 : -1}
         s = 0
-        for i,x in enumerate(nums):
-            s = (s + x)%k
+        for i in range(len(nums)):
+            s = (s+nums[i])%k
             if s not in hmap:
                 hmap[s] = i
-            elif i - hmap[s] > 1:
+            if i - hmap[s] > 1:
                 return True
         return False
+                 
 
 
