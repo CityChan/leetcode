2bd9509bc3e459e59be35156ffb7ca8bb7d53f7d
CityChan <53466807+CityChan@users.noreply.github.com>
2024-10-28 03:35:35 +0000

    [LeetCode Sync] Runtime - 7 ms (49.83%), Memory - 17.9 MB (95.26%)

--- my-folder/0207-course-schedule/solution.py
+++ my-folder/0207-course-schedule/solution.py
@@ -1,31 +1,22 @@
-class Solution(object):
-    def canFinish(self, numCourses, prerequisites):
-        """
-        :type numCourses: int
-        :type prerequisites: List[List[int]]
-        :rtype: bool
-        """
-        pre_dict = [set() for _ in range(numCourses)]
-        for i in prerequisites:
-            pre_dict[i[0]].add(i[1])
-        
-        visited = [0]*numCourses
-        
-        def dfs(course):
-            if visited[course]==1:
-                return True
-            if visited[course]==-1:
-                return False
-
-            visited[course]=-1
-            for c in pre_dict[course]:
-                if not dfs(c):
-                    return False
-            visited[course]=1
-            return True   
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        grid = [[] for _ in range(numCourses)]
+        for t,s in prerequisites:
+            grid[s].append(t)
+        self.onPath = [False for _ in range(numCourses)]
+        self.visited = [False for _ in range(numCourses)]
+        self.hasCycle = False
         for i in range(numCourses):
-            if not dfs(i):
-                return False
-        return True        
-                
-        
+            self.traverse(grid, i)
+        return not self.hasCycle
+
+    def traverse(self, grid, s):
+        if self.onPath[s]:
+            self.hasCycle = True
+        if self.visited[s] or self.hasCycle:
+            return    
+        self.visited[s] = True
+        self.onPath[s] = True
+        for t in grid[s]:
+            self.traverse(grid, t)
+        self.onPath[s] = False
