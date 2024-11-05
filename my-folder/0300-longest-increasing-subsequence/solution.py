5d93cf5b793b7630340413ceb29216395a64959b
CityChan <53466807+CityChan@users.noreply.github.com>
2024-11-05 22:11:12 +0000

    [LeetCode Sync] Runtime - 1756 ms (56.67%), Memory - 16.8 MB (97.33%)

--- my-folder/0300-longest-increasing-subsequence/solution.py
+++ my-folder/0300-longest-increasing-subsequence/solution.py
@@ -1,13 +1,14 @@
-class Solution(object):
-    def lengthOfLIS(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: int
-        """
-        res = [1]*len(nums)
-        for i in range(1, len(nums)):
+class Solution:
+    def lengthOfLIS(self, nums: List[int]) -> int:
+        n = len(nums)
+        dp = [1 for _ in range(n)]
+        for i in range(n):
             for j in range(i):
-                if nums[i]>nums[j]:
-                    res[i] = max(res[j]+1, res[i])
-        return max(res)
+                if nums[i] > nums[j]:
+                    dp[i] = max(dp[i], dp[j] + 1)
+        res = 0
+        for i in range(len(dp)):
+            res = max(res, dp[i])
+        return res
                 
+             
