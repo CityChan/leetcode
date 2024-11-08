7d96750ab1c3a697d1586e753a13c52baede2f5d
CityChan <53466807+CityChan@users.noreply.github.com>
2024-11-08 02:43:52 +0000

    [LeetCode Sync] Runtime - 23 ms (50.75%), Memory - 17.4 MB (87.85%)

---
+++ my-folder/1272-invalid-transactions/solution.py
@@ -0,0 +1,18 @@
+class Solution:
+    def invalidTransactions(self, transactions: List[str]) -> List[str]:
+        d = defaultdict(list)
+        idx = set()
+        for i, x in enumerate(transactions):
+            name, time, amount, city = x.split(',')
+            time, amount = int(time), int(amount)
+            d[name].append((time, city, i))
+            if amount > 1000:
+                idx.add(i)
+            for t,c,j in d[name]:
+                if c!= city and abs(time - t) <= 60:
+                    idx.add(j)
+                    idx.add(i)
+        return [transactions[i] for i in idx]
+
+
+
