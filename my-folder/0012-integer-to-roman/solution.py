d8c5ccaec24a60ac74947aab9ef6124408b13b54
CityChan <53466807+CityChan@users.noreply.github.com>
2024-11-08 03:55:32 +0000

    [LeetCode Sync] Runtime - 2 ms (83.89%), Memory - 16.7 MB (92.90%)

--- my-folder/0012-integer-to-roman/solution.py
+++ my-folder/0012-integer-to-roman/solution.py
@@ -1,17 +1,10 @@
-class Solution(object):
-    def intToRoman(self, num):
-        """
-        :type num: int
-        :rtype: str
-        """
-        char_list = ["M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"]
-        num_list = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1]
-        
-        res = ""
-        for i in range(len(char_list)):
-            n = num//num_list[i]
-            num = num-n*num_list[i]
-            res+=char_list[i]*n
-        return res
-
-        
+class Solution:
+    def intToRoman(self, num: int) -> str:
+        cs = ('M', 'CM', 'D', 'CD', 'C', 'XC', 'L', 'XL', 'X', 'IX', 'V', 'IV', 'I')
+        vs = (1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1)
+        ans = []
+        for c, v in zip(cs, vs):
+            while num >= v:
+                num -= v
+                ans.append(c)
+        return ''.join(ans)
