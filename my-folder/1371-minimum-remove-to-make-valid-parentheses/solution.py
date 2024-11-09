103b03bc1e15f391b17a86d0d07a666e5cbe6d3b
CityChan <53466807+CityChan@users.noreply.github.com>
2024-11-09 18:02:35 +0000

    [LeetCode Sync] Runtime - 74 ms (14.29%), Memory - 18.1 MB (90.61%)

--- my-folder/1371-minimum-remove-to-make-valid-parentheses/solution.py
+++ my-folder/1371-minimum-remove-to-make-valid-parentheses/solution.py
@@ -1,20 +1,21 @@
 class Solution:
     def minRemoveToMakeValid(self, s: str) -> str:
-        ans = ''
+        ans = ""
         left, right = 0, s.count(')')
         for c in s:
-            if c == '(':
-                if right > 0:
-                    ans += c
-                    left += 1
-                    right -= 1
-            elif c == ')':
+            if c == ')':
+                print(1)
                 if left > 0:
                     ans += c
                     left -= 1
                 else:
                     right -= 1
+            elif c == '(':
+                if right > 0:
+                    ans += c
+                    right -= 1
+                    left += 1
+
             else:
                 ans += c
-                
         return ans
