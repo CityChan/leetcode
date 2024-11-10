d25532a2602aa2a3a9397b831e0f8c185841c82a
CityChan <53466807+CityChan@users.noreply.github.com>
2024-11-10 01:17:22 +0000

    [LeetCode Sync] Runtime - 127 ms (90.29%), Memory - 17.2 MB (91.36%)

--- my-folder/1171-shortest-path-in-binary-matrix/solution.py
+++ my-folder/1171-shortest-path-in-binary-matrix/solution.py
@@ -10,15 +10,17 @@ class Solution:
         while q:
             for _ in range(len(q)):
                 i,j = q.popleft()
-                if i == j == n - 1:
+                if (i,j) == (n-1, n-1):
                     return ans
                 for d in dirs:
                     x, y = i + d[0], j + d[1]
-                    if 0 <= x < n and 0 <= y < n and grid[x][y] == 0:
-                        q.append((x, y))
+                    if x>=0 and x < n and y >=0 and y < n and grid[x][y] == 0:
                         grid[x][y] = 1
+                        q.append((x,y))
             ans += 1
+
         return -1
+
             
 
 
