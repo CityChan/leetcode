b9a44ed68558ed8a18f5f682e4739a8a29800658
CityChan <53466807+CityChan@users.noreply.github.com>
2024-11-10 02:22:20 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 16.9 MB (78.90%)

--- my-folder/0162-find-peak-element/solution.py
+++ my-folder/0162-find-peak-element/solution.py
@@ -1,10 +1,13 @@
+import heapq
 class Solution:
     def findPeakElement(self, nums: List[int]) -> int:
         left, right = 0, len(nums)-1
+        if len(nums) == 1:
+            return 0
         while left < right:
-            mid = (left+right)//2
-            if nums[mid] > nums[mid+1]:
-                right = mid
-            else:
+            mid = left + (right - left) // 2
+            if nums[mid] <= nums[mid+1]:
                 left = mid + 1
+            elif nums[mid] > nums[mid+1]:
+                right = mid
         return left
