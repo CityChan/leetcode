b4c1c6a3b40cbe480deecc60d73e8886862c69e7
CityChan <53466807+CityChan@users.noreply.github.com>
2024-11-11 16:50:50 +0000

    [LeetCode Sync] Runtime - 51 ms (36.78%), Memory - 30.2 MB (84.56%)

--- my-folder/1909-buildings-with-an-ocean-view/solution.py
+++ my-folder/1909-buildings-with-an-ocean-view/solution.py
@@ -1,12 +1,10 @@
 class Solution:
     def findBuildings(self, heights: List[int]) -> List[int]:
         stk = []
-        n = len(heights)
-        stk.append(n-1)
-        for i in range(n-2, -1, -1):
-            if heights[i] > heights[stk[-1]]:
-                stk.append(i)
-        stk.sort()
+        for i,h in enumerate(heights):
+            while stk and heights[stk[-1]] <= h:
+                stk.pop()
+            stk.append(i)
         return stk
                 
             
