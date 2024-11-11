16002154659470b50af96ce3e910b3681192a0de
CityChan <53466807+CityChan@users.noreply.github.com>
2024-11-11 17:05:51 +0000

    [LeetCode Sync] Runtime - 120 ms (78.03%), Memory - 78.4 MB (33.66%)

--- my-folder/0146-lru-cache/solution.py
+++ my-folder/0146-lru-cache/solution.py
@@ -2,28 +2,27 @@ class LRUCache:
 
     def __init__(self, capacity: int):
         self.cap = capacity
-        self.cache = {}
-        self.used = []
+        self.cache = collections.OrderedDict()
+
     def get(self, key: int) -> int:
         if key in self.cache:
-            self.used.remove(key)
-            self.used.append(key)
-            return self.cache[key]
+            answer =  self.cache[key]
+            self.cache.pop(key)
+            self.cache[key] = answer
+            return answer
         else:
             return -1
 
     def put(self, key: int, value: int) -> None:
         if key in self.cache:
-            self.used.remove(key)
-            self.used.append(key)
+            self.cache.pop(key)
             self.cache[key] = value
         else:
-            while len(self.cache) >= self.cap:
-                delete_key = self.used.pop(0)
-                del self.cache[delete_key]
-            self.used.append(key)
+            if len(self.cache) >= self.cap:
+                ordest_key = next(iter(self.cache))
+                self.cache.pop(ordest_key)
             self.cache[key] = value
-        
+
 
 
 # Your LRUCache object will be instantiated and called as such:
