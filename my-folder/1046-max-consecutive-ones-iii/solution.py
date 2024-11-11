f244a685b4b2789880060083741041767304baeb
CityChan <53466807+CityChan@users.noreply.github.com>
2024-11-11 16:42:35 +0000

    [LeetCode Sync] Runtime - 427 ms (5.02%), Memory - 17.7 MB (70.19%)

--- my-folder/1046-max-consecutive-ones-iii/solution.py
+++ my-folder/1046-max-consecutive-ones-iii/solution.py
@@ -1,20 +1,20 @@
 class Solution:
     def longestOnes(self, nums: List[int], k: int) -> int:
-        ones = sum(nums)
         backup = k
         max_window = 0
         left, right = 0, 0
         while right < len(nums):
+            print(left, right, backup)
             if nums[right] == 1:
                 right += 1
             else:
                 if backup > 0:
-                    right += 1
                     backup -= 1
+                    right += 1
                 else:
                     max_window = max(max_window, right - left)
                     if nums[left] == 0:
-                        backup +=1
+                            backup +=1
                     left += 1
         max_window = max(max_window, right - left)
         return max_window
