310727e00cd89fc892694b4ce111424f8c5c9da9
CityChan <53466807+CityChan@users.noreply.github.com>
2024-11-11 01:47:34 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 16.7 MB (86.03%)

--- my-folder/0031-next-permutation/solution.py
+++ my-folder/0031-next-permutation/solution.py
@@ -6,17 +6,18 @@ class Solution:
         n = len(nums)
         i = -1
         j = -1
-        for k in range(n-1,0,-1):
-            if nums[k-1] < nums[k]:
-                i = k - 1
+        for k in range(n-2, -1, -1):
+            if nums[k] < nums[k + 1]:
+                i = k
                 break
         if i >= 0:
-            for k in range(n-1,-1,-1):
+            for k in range(n-1, -1, -1):
                 if nums[k] > nums[i]:
                     j = k
                     break
-            temp = nums[i]
-            nums[i] =nums[j]
-            nums[j] = temp
+
+        temp = nums[i]
+        nums[i] = nums[j]
+        nums[j] = temp
         nums[i+1:] = nums[i+1:][::-1]
         return nums
