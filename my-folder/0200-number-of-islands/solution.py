113683d0505dd1f5d4474d978b56e287b86b56fa
CityChan <53466807+CityChan@users.noreply.github.com>
2024-11-14 04:51:26 +0000

    [LeetCode Sync] Runtime - 244 ms (61.72%), Memory - 19.1 MB (89.99%)

--- my-folder/0200-number-of-islands/solution.py
+++ my-folder/0200-number-of-islands/solution.py
@@ -1,24 +1,20 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        m,n = len(grid), len(grid[0])
-        visited = [[0 for _ in range(n)] for _ in range(m)]
-        dirs = [[1,0], [0,1], [-1, 0], [0, -1]]
-        def dps(grid, i, j):
-            visited[i][j] = 1
-            grid[i][j] = '0'
-            for d in dirs:
-                x, y = i + d[0], j + d[1]
-                if x < 0 or x >= m or y < 0 or y >= n:
-                    continue
-                if visited[x][y] == 0 and grid[x][y] == '1':
-                    dps(grid, x,y)
-        ans = 0
-        for k in range(m):
-            for l in range(n):
-                if k < 0 or k >= m or l < 0 or l >= n or grid[k][l] == '0':
-                    continue
-                ans +=1
-                dps(grid, k, l)
-        return ans
-
-
+        m, n = len(grid), len(grid[0])
+        res = 0
+        for i in range(m):
+            for j in range(n):
+                if grid[i][j] == "1":
+                    res += 1
+                    self.dfs(grid, i,j)
+        return res
+    def dfs(self, grid, i, j):
+        m, n = len(grid), len(grid[0])
+        if i < 0 or i >= m or j < 0 or j >= n or grid[i][j] == "0":
+            return 
+        grid[i][j] = "0"
+        dirs = [[0,1], [1,0], [0,-1], [-1,0]]
+        for d in dirs:
+            x, y = i + d[0], j + d[1]
+            self.dfs(grid, x, y)
+        
