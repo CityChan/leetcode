1413c523d358d7187c09d1b6113f4eca7a271212
CityChan <53466807+CityChan@users.noreply.github.com>
2024-11-14 01:33:58 +0000

    [LeetCode Sync] Runtime - 48 ms (7.79%), Memory - 19.3 MB (96.89%)

--- my-folder/0560-subarray-sum-equals-k/solution.py
+++ my-folder/0560-subarray-sum-equals-k/solution.py
@@ -1,9 +1,9 @@
 class Solution:
     def subarraySum(self, nums: List[int], k: int) -> int:
-        count = Counter({0:1})
+        hashmap = Counter({0:1})
         ans, acc = 0, 0
         for num in nums:
             acc += num
-            ans += count[acc - k]
-            count[acc] += 1
+            ans += hashmap[acc - k]
+            hashmap[acc] += 1
         return ans
