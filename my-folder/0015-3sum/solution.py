f28a1f76e1536d41de0a9a93c30b242ff35ce241
CityChan <53466807+CityChan@users.noreply.github.com>
2024-11-14 03:53:03 +0000

    [LeetCode Sync] Runtime - 898 ms (22.41%), Memory - 20.3 MB (50.46%)

--- my-folder/0015-3sum/solution.py
+++ my-folder/0015-3sum/solution.py
@@ -9,29 +9,28 @@ class Solution:
         if n < 2 or sz < n:
             return res
         if n == 2:
-            lo, hi = start, sz - 1
+            lo, hi = start, sz -1
             while lo < hi:
                 sum_val = nums[lo] + nums[hi]
                 left, right = nums[lo], nums[hi]
                 if sum_val < target:
                     while lo < hi and nums[lo] == left:
                         lo += 1
-                elif sum_val > target:
+                elif  sum_val > target:
                     while lo < hi and nums[hi] == right:
                         hi -= 1
                 else:
-                    res.append([left,right])
-                    while lo < hi and nums[lo] == right:
+                    res.append([left, right])
+                    while lo < hi and nums[lo] == left:
                         lo += 1
                     while lo < hi and nums[hi] == right:
-                        hi -= 1                    
+                        hi -= 1
         else:
             for i in range(start, sz):
                 if i > start and nums[i] == nums[i - 1]:
                     continue
-                sub = self.nSumTarget(nums, n-1, i+1, target - nums[i])
+                sub = self.nSumTarget(nums, n-1, i+1, target - nums[i] )
                 for arr in sub:
                     arr.append(nums[i])
                     res.append(arr)
         return res
-
