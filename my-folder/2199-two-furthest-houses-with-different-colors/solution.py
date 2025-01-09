2e72cd1a21cce33aaa55c1f6dddf4ba52b5caaf1
CityChan <53466807+CityChan@users.noreply.github.com>
2025-01-09 17:45:45 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 17.7 MB (32.98%)

---
+++ my-folder/2199-two-furthest-houses-with-different-colors/solution.py
@@ -0,0 +1,14 @@
+class Solution:
+    def maxDistance(self, colors: List[int]) -> int:
+        n = len(colors)
+        if colors[0] != colors[-1]:
+            return n - 1
+        i, j = 1, n - 2
+        while colors[i] == colors[0]:
+            i += 1
+        while colors[j] == colors[0]:
+            j -= 1
+        return max(j, n - i - 1)
+        
+
+        
