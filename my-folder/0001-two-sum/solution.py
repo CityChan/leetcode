7684df6ca468af2c8cd392ba7243b467d45ec8b9
CityChan <53466807+CityChan@users.noreply.github.com>
2025-01-22 15:19:20 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 19 MB (23.73%)

--- my-folder/0001-two-sum/solution.py
+++ my-folder/0001-two-sum/solution.py
@@ -1,67 +1,12 @@
-class Solution(object):
-    def twoSum(self, nums, target):
-        """
-        :type nums: List[int]
-        :type target: int
-        :rtype: List[int]
-        """
-        save_list = [None]*len(nums)
-        for i, n in enumerate(nums):
-            save_list[i] = target - n
-        for i, n in enumerate(nums):
-            try:
-                find_idx = save_list.index(n)
-                if i != find_idx:
-                    return [i, find_idx]
-            except:
-                continue
-        
-        # save_dict = {}
-        # for i, n in enumerate(nums):
-        #     save_dict[target-n] = i
-        # for i, n in enumerate(nums):
-        #     if n in save_dict and save_dict[n]!=i:
-        #         return [i, save_dict[n]]
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-#         save_dict = {}
-#         for i, n in enumerate(nums):
-#             save_dict[target-n]=i
-        
-#         for i, n in enumerate(nums):
-#             if n in save_dict:
-#                 if i!=save_dict[n]:
-#                     return [i, save_dict[n]]
-            
-        
-        
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        n = len(nums)
+        hashmap = {}
+        for i in range(n):
+            need = target - nums[i]
+            if need in hashmap:
+                return [hashmap[need], i]
+            hashmap[nums[i]] = i
+        return []
+
+
